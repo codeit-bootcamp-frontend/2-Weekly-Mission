298fe991a03d7c404959151509d06f3586a6cf14
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-11-08 19:47:22 +0900

    Test: 입력 폼 내부 input 요소의 focusin out 이벤트 작동 테스트

--- js/signin.js
+++ js/signin.js
@@ -1,6 +1,23 @@
 const submitBtn = document.querySelector(".btn-long-40");
 const emailInput = document.querySelector("#input-id");
 const passwordInput = document.querySelector("#input-pwd");
+const inputForm = document.querySelector(".input-container");
+
+inputForm.addEventListener("focusout", function (e) {
+  if (e.target.id === "input-id") {
+    console.log("이메일 창에서 focusout 함");
+  } else if (e.target.id === "input-pwd") {
+    console.log("비밀번호 창에서 focusout 함");
+  }
+});
+
+inputForm.addEventListener("focusin", function (e) {
+  if (e.target.id === "input-id") {
+    console.log("이메일 창에 focusin 함");
+  } else if (e.target.id === "input-pwd") {
+    console.log("비밀번호 창에 focusin 함");
+  }
+});
 
 submitBtn.addEventListener("click", function (e) {
   if (
