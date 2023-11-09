5f3e9afb77f06aa712bf8f44c4be0c41ac505145
Ohreum <dhfma3394@naver.com>
2023-11-09 16:20:09 +0900

    feat: 로그인 버튼 클릭시 검증 로직 추가

--- src/signin.js
+++ src/signin.js
@@ -1,36 +1,51 @@
-import { ERROR_MESSAGE, USER_EMAIL } from './../constants.js';
+import { ERROR_MESSAGE, USER_EMAIL, USER_PASSWORDS } from './../constants.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   let emailInput = document.querySelector('.sign-input[type="email"]');
   let passwordInput = document.querySelector('.sign-input[type="password');
   let warningMessage = document.querySelectorAll('.warning-message');
+  let loginBtn = document.querySelector('.login-btn');
 
   emailInput.addEventListener('focusout', vaildateEmail);
   passwordInput.addEventListener('focusout', vaildatePassWord);
+  loginBtn.addEventListener('click', performLogin);
 
   function vaildateEmail() {
     if (emailInput.value != '' && isEmail(emailInput.value)) {
       emailInput.style.borderColor = '#ccd5e3';
       warningMessage[0].innerHTML = '';
     } else if (emailInput.value == '') {
-      printErrorMessage(ERROR_MESSAGE.emptyEmail);
+      printErrorMessage(ERROR_MESSAGE.emptyEmail, 0);
     } else if (!isEmail(emailInput.value)) {
-      printErrorMessage(ERROR_MESSAGE.isNotVaildEmail);
+      printErrorMessage(ERROR_MESSAGE.isNotVaildEmail, 0);
     }
   }
 
   function vaildatePassWord() {
     if (passwordInput.value == '') {
-      let html = `<p class="warning-message">${ERROR_MESSAGE.emptyPassWord}</p>`;
-      passwordInput.style.borderColor = '#ff5b56';
-      warningMessage[1].innerHTML = html;
+      printErrorMessage(ERROR_MESSAGE.emptyPassWord, 1);
     }
   }
 
-  function printErrorMessage(message) {
+  function printErrorMessage(message, index) {
     let html = `<p class="warning-message">${message}</p>`;
     emailInput.style.borderColor = '#ff5b56';
-    warningMessage[0].innerHTML = html;
+    warningMessage[index].innerHTML = html;
+  }
+
+  function performLogin(e) {
+    e.preventDefault();
+
+    if (emailInput.value != USER_EMAIL && passwordInput.value != USER_PASSWORDS) {
+      let wrongEmail = `<p class="warning-message">${ERROR_MESSAGE.wrongEmail}</p>`;
+      let wrongPassWord = `<p class="warning-message">${ERROR_MESSAGE.wrongPassWord}</p>`;
+
+      emailInput.style.borderColor = '#ff5b56';
+      passwordInput.style.borderColor = '#ff5b56';
+
+      warningMessage[0].innerHTML = wrongEmail;
+      warningMessage[1].innerHTML = wrongPassWord;
+    }
   }
 
   function isEmail(asValue) {
