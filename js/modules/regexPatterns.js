ffb3fd38ffcd0b5f0bc962ffacb1a7101a47f81f
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-11-09 13:51:20 +0900

    Refactor: 정규식 변수 regexPattern.js 에 모듈화

---
+++ js/modules/regexPatterns.js
@@ -0,0 +1,3 @@
+const regEmail = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+
+export default regEmail;
--- js/signin.js
+++ js/signin.js
@@ -1,6 +1,5 @@
 import { submitBtn, emailInput, passwordInput, inputForm } from "./modules/domSelectors.js";
-
-const regEmail = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+import regEmail from "./modules/regexPatterns.js";
 
 inputForm.addEventListener("focusout", function (e) {
   if (e.target.id === "input-id") {
