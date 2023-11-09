e831bacd9f899de8840ba0ed9981fa0571a37d7c
Ohreum <dhfma3394@naver.com>
2023-11-09 19:09:02 +0900

    refactor: 중복되는 코드 함수화

--- src/signin.js
+++ src/signin.js
@@ -3,61 +3,65 @@ import { ERROR_MESSAGE, USER_EMAIL, USER_PASSWORD } from './../constants.js';
 document.addEventListener('DOMContentLoaded', () => {
   let emailInput = document.querySelector('.sign-input[type="email"]');
   let passwordInput = document.querySelector('.sign-input[type="password');
-  let warningMessage = document.querySelectorAll('.warning-message');
+  let emailWarningMessage = document.querySelector('.email-warning-message');
+  let passwordWarningMessage = document.querySelector('.password-warning-message');
   let loginBtn = document.querySelector('.login-btn');
   let eyeBtn = document.querySelector('.eye-button');
   let eyeBtnIcon = eyeBtn.firstElementChild;
 
-  emailInput.addEventListener('focusout', vaildateEmail);
-  passwordInput.addEventListener('focusout', vaildatePassWord);
+  emailInput.addEventListener('focusout', validateEmail);
+  passwordInput.addEventListener('focusout', validatePassWord);
   loginBtn.addEventListener('click', performLogin);
   eyeBtn.addEventListener('click', showAndHidePassword);
 
-  function vaildateEmail() {
+  function validateEmail() {
     if (emailInput.value != '' && isEmail(emailInput.value)) {
-      emailInput.style.borderColor = '#ccd5e3';
-      warningMessage[0].innerHTML = '';
+      //코드 상단에 선언한 DOM 파라미터로 전달
+      resetErrorMessage(emailInput, emailWarningMessage);
     } else if (emailInput.value == '') {
-      printErrorMessage(ERROR_MESSAGE.emptyEmail, 0);
+      printErrorMessage(emailInput, emailWarningMessage, ERROR_MESSAGE.emptyEmail);
     } else if (!isEmail(emailInput.value)) {
-      printErrorMessage(ERROR_MESSAGE.isNotVaildEmail, 0);
+      printErrorMessage(emailInput, emailWarningMessage, ERROR_MESSAGE.isNotVaildEmail);
     }
   }
 
-  function vaildatePassWord() {
+  function validatePassWord() {
     if (passwordInput.value == '') {
-      let html = `<p class="warning-message">${ERROR_MESSAGE.emptyPassWord}</p>`;
-      passwordInput.style.borderColor = '#ff5b56';
-      warningMessage[1].innerHTML = html;
+      printErrorMessage(passwordInput, passwordWarningMessage, ERROR_MESSAGE.emptyPassWord);
     }
-  }
-
-  function printErrorMessage(message, index) {
-    let html = `<p class="warning-message">${message}</p>`;
-    emailInput.style.borderColor = '#ff5b56';
-    warningMessage[index].innerHTML = html;
   }
 
   function performLogin(e) {
     e.preventDefault();
 
-    if (emailInput.value != USER_EMAIL || passwordInput.value != USER_PASSWORD) {
-      if (emailInput.value != USER_EMAIL) {
-        let wrongEmail = `<p class="warning-message">${ERROR_MESSAGE.wrongEmail}</p>`;
-        emailInput.style.borderColor = '#ff5b56';
-        warningMessage[0].innerHTML = wrongEmail;
-      }
-      if (passwordInput.value != USER_PASSWORD) {
-        let wrongPassWord = `<p class="warning-message">${ERROR_MESSAGE.wrongPassWord}</p>`;
-        passwordInput.style.borderColor = '#ff5b56';
-        warningMessage[1].innerHTML = wrongPassWord;
-      } else if (passwordInput.value != '' || passwordInput.value == USER_PASSWORD) {
-        passwordInput.style.borderColor = '#ccd5e3';
-        warningMessage[1].innerHTML = '';
-      }
-    } else {
+    if (emailInput.value != USER_EMAIL) {
+      printErrorMessage(emailInput, emailWarningMessage, ERROR_MESSAGE.wrongEmail);
+    }
+    if (passwordInput.value != USER_PASSWORD) {
+      printErrorMessage(passwordInput, passwordWarningMessage, ERROR_MESSAGE.wrongPassWord);
+    }
+    if (passwordInput.value != '' && passwordInput.value == USER_PASSWORD) {
+      resetErrorMessage(passwordInput, passwordWarningMessage);
+    }
+    if (emailInput.value === USER_EMAIL && passwordInput.value === USER_PASSWORD) {
       window.location.href = '/folder.html';
     }
+  }
+
+  // warningMessage가 HTML 요소인 HTMLParagraphElement 객체를 가리키므로
+  // warningMessage.className을 통해
+  // email-warning-message 혹은 password-warning-message 값을 바인딩
+  function printErrorMessage(input, warningMessage, message) {
+    let html = `<p class="${warningMessage.className}">${message}</p>`;
+
+    input.style.borderColor = '#ff5b56';
+    warningMessage.innerHTML = html;
+    warningMessage.style.display = 'block';
+  }
+
+  function resetErrorMessage(input, warningMessage) {
+    input.style.borderColor = '#ccd5e3';
+    warningMessage.innerHTML = '';
   }
 
   function showAndHidePassword() {
