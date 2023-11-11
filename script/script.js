629a1bff0aa981900e6fc9f6b9f5b7e5e1a18836
김우현 <rladngus133@naver.com>
2023-11-11 16:01:25 +0900

    feat: 이메일 검사기능 구현완료

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
+  // =================================================================
   const emailInput = document.querySelector("#email");
   const pwInput = document.querySelector("#password");
   const emailErrorMsg = document.querySelector("#email_error");
@@ -9,7 +10,18 @@ document.addEventListener("DOMContentLoaded", function () {
     if (emailInput.value === "") {
       emailErrorMsg.classList.remove("done");
       emailErrorMsg.classList.add("show");
+      emailErrorMsg.innerHTML = "이메일 주소를 입력하세요.";
       emailInput.classList.add("errorInput_style");
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailInput.value)) {
+      emailErrorMsg.innerHTML = "올바른 이메일 주소가 아닙니다.";
+    } else if (emailInput.value === "test@codeit.com") {
+      emailErrorMsg.innerHTML = "이미 사용 중인 이메일입니다.";
+    } else {
+      emailErrorMsg.classList.remove("show");
+      emailErrorMsg.classList.add("done");
+      emailInput.classList.remove("errorInput_style");
     }
   });
+
+  // =================================================================
 });
