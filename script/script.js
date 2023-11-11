1676752ba5fd86aa9c58a80ef5b2acfc7b54fe56
김우현 <rladngus133@naver.com>
2023-11-11 15:37:15 +0900

    feat: 오류메시지출력 기능구현 테스트(성공)

--- script/script.js
+++ script/script.js
@@ -2,5 +2,15 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   const emailInput = document.querySelector("#email");
-  const passwordInput = document.querySelector("#password");
+  const pwInput = document.querySelector("#password");
+  const emailDiv = document.querySelector(".email_container");
+  const pwDiv = document.querySelector(".password_container");
+  const emailErrorMsg = document.querySelector("#email_error");
+
+  emailInput.addEventListener("blur", () => {
+    if (emailInput.value === "") {
+      emailErrorMsg.classList.remove("done");
+      emailErrorMsg.classList.add("show");
+    }
+  });
 });
