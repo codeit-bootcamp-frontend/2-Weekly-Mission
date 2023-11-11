a5c209a4adebc81bce434f3d9aa01ba5f68fe76a
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-11-12 00:35:53 +0900

    <feat>: 로그인 기능 테스트하는 함수 구현 및 이벤트 할당
    signin.js에 로그인 기능을 테스트 하는 함수 구현 및 로그인 버튼에 이벤트 할당
    folder.html생성
    signin.html에서 로그인 버튼의 type을 button으로 수정하고 form에 아이디 설정
    그 외 signin.js에서 checkEmail 메소드 수정(하다보니 커밋 단위가 넘어갔습니다..)

--- js/signin.js
+++ js/signin.js
@@ -1,30 +1,61 @@
 import makeDOM from "./makeDOM.js";
 
 const emailInput = document.querySelector('#email');
+const signInBtn = document.querySelector('.signin--btn');
 
 const checkEmail = (e) => {
   if(e.target === emailInput) {
-    console.log(`'event.target = ${e.target}`);
+    if(e.target.value === '') return;
     const emailValue = e.target.value.trim();
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if (emailRegex.test(emailValue)){
-      emailInput.classList.remove('input--error');
-      emailInput.parentNode.removeChild(emailInput.parentNode.lastChild);
+    if (emailRegex.test(emailValue)) {
+      if(e.target.parentNode.lastChild.classList.contains('error-message')) {
+        emailInput.parentNode.removeChild(emailInput.parentNode.lastChild);
+        emailInput.classList.remove('input--error')
+      }
+      else return;
     }
     else {
-      if(e.target.value === '') return;
-      else {
-        const errorMessage = makeDOM('p', {
-          className: 'email-error error-message',
-          innerText: '올바른 이메일 주소가 아닙니다.'
-        })
-        if(document.querySelector('.error-message')) return;
-        emailInput.parentNode.appendChild(errorMessage);
-        emailInput.classList.add('input--error');
-      }
+      const errorMessage = makeDOM('p', {
+        className: 'email-error error-message',
+        innerText: '올바른 이메일 주소가 아닙니다.'
+      })
+      if(document.querySelector('.error-message')) return;
+      emailInput.parentNode.appendChild(errorMessage);
+      emailInput.classList.add('input--error');
     } 
+  }
+};
+const signInTest = (e) => {
+  e.preventDefault();
+  if(e.target === document.querySelector('.signin--btn')){
+    const emailInput = document.querySelector('#email');
+    const passwordInput = document.querySelector('#password');
+
+    if(emailInput.value !== 'test@codeit.com') {
+      const errorMessage = makeDOM('p', {
+        className: 'email-error error-message',
+        innerText: '이메일을 확인해주세요.'
+      })
+      // if(document.querySelector('.error-message')) return;
+      emailInput.parentNode.appendChild(errorMessage);
+      emailInput.classList.add('input--error');
+    }
+    if(passwordInput.value !== 'codeit101') {
+      const errorMessage = makeDOM('p', {
+        className: 'password-error error-message',
+        innerText: '비밀번호를 확인해주세요.'
+      })
+      // if(document.querySelector('.error-message')) return;
+      passwordInput.parentNode.appendChild(errorMessage);
+      passwordInput.classList.add('input--error');
+    }
+    if(emailInput.value === 'test@codeit.com' && passwordInput.value === 'codeit101'){
+      window.location.href = './folder.html';
+    }
   }
 };
 
 emailInput.addEventListener('input', checkEmail);
 emailInput.addEventListener('blur', checkEmail);
+signInBtn.addEventListener('click', signInTest);
