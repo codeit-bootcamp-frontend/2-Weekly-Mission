1677b4b4b09db172bb5a9c59a92d49f49cc8346c
김우현 <rladngus133@naver.com>
2023-11-12 00:12:12 +0900

    feat: 회원가입 기능구현
    - 회원가입 기능구현을 하였으나 추후 JS 스킬을 더 습득하면
      각각의 검사로직을 함수로 대체하여 마지막의 signupBtn 이벤트를
      더 가독성 좋게 짜면 어떨까싶음

--- script/signup.js
+++ script/signup.js
@@ -89,6 +89,54 @@ document.addEventListener("DOMContentLoaded", function () {
     pwCheckedVisibleToggle = !pwCheckedVisibleToggle;
   });
 
-  signUpBtn.addEventListener("submit", () => {});
+  function signupHandler(e) {
+    if (emailInput.value === "") {
+      showError(emailErrorMsg, emailInput, "이메일 주소를 입력하세요.");
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailInput.value)) {
+      showError(emailErrorMsg, emailInput, "올바른 이메일 주소가 아닙니다.");
+    } else if (emailInput.value === "test@codeit.com") {
+      showError(emailErrorMsg, emailInput, "이미 사용 중인 이메일입니다.");
+    } else {
+      doneError(emailErrorMsg, emailInput);
+    }
+
+    if (pwInput.value === "") {
+      showError(pwErrorMsg, pwInput, "비밀번호를 입력해주세요.");
+    } else if (
+      pwInput.value.length < 8 ||
+      /^[a-zA-Z]+$/.test(pwInput.value) ||
+      /^\d+$/.test(pwInput.value)
+    ) {
+      showError(
+        pwErrorMsg,
+        pwInput,
+        "비밀번호는 8자 이상이어야 하며, 문자와 숫자를 혼합해야 합니다."
+      );
+    } else {
+      doneError(pwErrorMsg, pwInput);
+    }
+
+    if (pwInput.value !== pwCheckInput.value) {
+      showError(pwCheckErrorMsg, pwCheckInput, "비밀번호가 일치하지 않아요.");
+    } else {
+      doneError(pwCheckErrorMsg, pwCheckInput);
+    }
+
+    if (
+      emailErrorMsg.classList.contains("show") ||
+      pwErrorMsg.classList.contains("show") ||
+      pwCheckErrorMsg.classList.contains("show")
+    ) {
+      e.preventDefault();
+    }
+
+    console.log("Success signup");
+  }
+
+  signUpBtn.addEventListener("click", signupHandler);
+  signUpBtn.addEventListener("keyup", (e) => {
+    e.keyCode === 13 && signupHandler(e);
+  });
+
   // =================================================================
 });
