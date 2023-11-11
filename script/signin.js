a1d70cf859ae6c829560ceee83dc2a4fb0f95327
김우현 <rladngus133@naver.com>
2023-11-11 18:43:41 +0900

    refactor: 코드 성능향상 및 가독성유지 리팩토링

--- script/signin.js
+++ script/signin.js
@@ -10,49 +10,46 @@ document.addEventListener("DOMContentLoaded", function () {
 
   let pwVisibleToggle = true;
 
+  function showError(errorMsg, input, message) {
+    errorMsg.classList.remove("done");
+    errorMsg.classList.add("show");
+    errorMsg.innerHTML = message;
+    input.classList.add("errorInput_style");
+  }
+
+  function doneError(errorMsg, input) {
+    errorMsg.classList.remove("show");
+    errorMsg.classList.add("done");
+    input.classList.remove("errorInput_style");
+  }
+
   emailInput.addEventListener("blur", () => {
     if (emailInput.value === "") {
-      emailErrorMsg.classList.remove("done");
-      emailErrorMsg.classList.add("show");
-      emailErrorMsg.innerHTML = "이메일 주소를 입력하세요.";
-      emailInput.classList.add("errorInput_style");
+      showError(emailErrorMsg, emailInput, "이메일 주소를 입력하세요.");
     } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailInput.value)) {
-      emailErrorMsg.classList.remove("done");
-      emailErrorMsg.classList.add("show");
-      emailErrorMsg.innerHTML = "올바른 이메일 주소가 아닙니다.";
-      emailInput.classList.add("errorInput_style");
+      showError(emailErrorMsg, emailInput, "올바른 이메일 주소가 아닙니다.");
     } else if (emailInput.value === "test@codeit.com") {
-      emailErrorMsg.classList.remove("done");
-      emailErrorMsg.classList.add("show");
-      emailErrorMsg.innerHTML = "이미 사용 중인 이메일입니다.";
-      emailInput.classList.add("errorInput_style");
+      showError(emailErrorMsg, emailInput, "이미 사용 중인 이메일입니다.");
     } else {
-      emailErrorMsg.classList.remove("show");
-      emailErrorMsg.classList.add("done");
-      emailInput.classList.remove("errorInput_style");
+      doneError(emailErrorMsg, emailInput);
     }
   });
 
   pwInput.addEventListener("blur", () => {
     if (pwInput.value === "") {
-      pwErrorMsg.classList.remove("done");
-      pwErrorMsg.classList.add("show");
-      pwErrorMsg.innerHTML = "비밀번호를 입력해주세요.";
-      pwInput.classList.add("errorInput_style");
+      showError(pwErrorMsg, pwInput, "비밀번호를 입력해주세요.");
     } else if (
       pwInput.value.length < 8 ||
       /^[a-zA-Z]+$/.test(pwInput.value) ||
       /^\d+$/.test(pwInput.value)
     ) {
-      pwErrorMsg.classList.remove("done");
-      pwErrorMsg.classList.add("show");
-      pwErrorMsg.innerHTML =
-        "이메일 주소는 8자 이상이어야 하며, 문자와 숫자를 혼합해야 합니다.";
-      pwInput.classList.add("errorInput_style");
+      showError(
+        pwErrorMsg,
+        pwInput,
+        "비밀번호는 8자 이상이어야 하며, 문자와 숫자를 혼합해야 합니다."
+      );
     } else {
-      pwErrorMsg.classList.remove("show");
-      pwErrorMsg.classList.add("done");
-      pwInput.classList.remove("errorInput_style");
+      doneError(pwErrorMsg, pwInput);
     }
   });
 
