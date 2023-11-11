29d12e4daea10cc01d2c9d1effec607c37e75834
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-11-11 20:55:09 +0900

    <feat>: 비밀번호의 조합이 영문과 숫자이고 8자이상인지 판별하는 함수 구현 및 이벤트 할당

--- js/signup.js
+++ js/signup.js
@@ -8,6 +8,7 @@ const confirmPassword = (e) => {
     if(passwordInput.value === passwordCheckInput.value) return;
     else {
       if(e.target.value === '') return;
+      if(e.target.parentNode.lastChild.classList.contains('error-message')) return;
       const errorMessage = makeDOM('p', {
         className:'password-error error-message',
         innerText: '비밀번호가 일치하지 않아요',
@@ -18,4 +19,20 @@ const confirmPassword = (e) => {
   }
 }
 
-passwordCheckInput.addEventListener('focusout', confirmPassword)
+const checkPassword = (e) => {
+  if(e.target.id === 'password'){
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
+    if (passwordRegex.test(e.target.value)) return;
+    else {
+      if(e.target.value === '') return;
+      const errorMessage = makeDOM('p', {
+        className:'password-error error-message',
+        innerText: '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.',
+      })
+      e.target.parentNode.appendChild(errorMessage);
+      e.target.classList.add('input--error');
+  }
+  }
+}
+passwordCheckInput.addEventListener('focusout', confirmPassword);
+passwordInput.addEventListener('focusout', checkPassword);
