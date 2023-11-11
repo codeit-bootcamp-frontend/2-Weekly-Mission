38560c3fb33e20088be282dc3e10357b6b21b470
김우현 <rladngus133@naver.com>
2023-11-11 16:11:01 +0900

    feat: 비밀번호 검사기능구현 완료

--- script/script.js
+++ script/script.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const emailInput = document.querySelector("#email");
   const pwInput = document.querySelector("#password");
   const emailErrorMsg = document.querySelector("#email_error");
+  const pwErrorMsg = document.querySelector("#pw_error");
 
   emailInput.addEventListener("blur", () => {
     if (emailInput.value === "") {
@@ -19,6 +20,24 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
       emailErrorMsg.classList.remove("show");
       emailErrorMsg.classList.add("done");
+      emailInput.classList.remove("errorInput_style");
+    }
+  });
+
+  pwInput.addEventListener("blur", () => {
+    if (
+      pwInput.value.length < 8 ||
+      /^[a-zA-Z]+$/.test(pwInput.value) ||
+      /^\d+$/.test(pwInput.value)
+    ) {
+      pwErrorMsg.classList.remove("done");
+      pwErrorMsg.classList.add("show");
+      pwErrorMsg.innerHTML =
+        "이메일 주소는 8자 이상이어야 하며, 문자와 숫자를 혼합해야 합니다.";
+      emailInput.classList.add("errorInput_style");
+    } else {
+      pwErrorMsg.classList.remove("show");
+      pwErrorMsg.classList.add("done");
       emailInput.classList.remove("errorInput_style");
     }
   });
