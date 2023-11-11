5c0f58b71005312436bf5dd899f65880ff277d8e
김우현 <rladngus133@naver.com>
2023-11-11 14:20:16 +0900

    feat: 인풋요소 변수추가 및 signin.html 연결

--- script/script.js
+++ script/script.js
@@ -1,1 +1,6 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const emailInput = document.querySelector("#email");
+  const passwordInput = document.querySelector("#password");
+});
