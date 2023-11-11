8f16bcf60ffa1bf42666e19197db12e802e2036f
김우현 <rladngus133@naver.com>
2023-11-11 15:41:59 +0900

    fstyle: 클래스이름 수정 및 추가

--- script/script.js
+++ script/script.js
@@ -3,14 +3,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const emailInput = document.querySelector("#email");
   const pwInput = document.querySelector("#password");
-  const emailDiv = document.querySelector(".email_container");
-  const pwDiv = document.querySelector(".password_container");
   const emailErrorMsg = document.querySelector("#email_error");
 
   emailInput.addEventListener("blur", () => {
     if (emailInput.value === "") {
       emailErrorMsg.classList.remove("done");
       emailErrorMsg.classList.add("show");
+      emailInput.classList.add("errorInput_style");
     }
   });
 });
