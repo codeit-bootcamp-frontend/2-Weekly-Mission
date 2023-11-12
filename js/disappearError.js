df430d64a3c5f32a2a7d863f6db8b1170fcac6b5
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-11-12 15:31:27 +0900

    <refactor>: 모듈로 사용할 때 disappearError 함수내에 중복되거나 필요없는 코드 삭제

--- js/disappearError.js
+++ js/disappearError.js
@@ -1,12 +1,9 @@
 const disappearError = (tag) => {
-  const inputList = Array.from(document.querySelectorAll('input'));
-  if(inputList.includes(tag)) {
-    tag.classList.remove('input--error');
-    const parent = tag.parentNode;
-    const lastNode = tag.parentNode.lastChild;
-    if(lastNode.classList.contains('error-message')){
-      parent.removeChild(lastNode);
-    }
+  tag.classList.remove('input--error');
+  const parent = tag.parentNode;
+  const lastNode = tag.parentNode.lastChild;
+  if(lastNode.classList.contains('error-message')){
+    parent.removeChild(lastNode);
   }
 }
 
