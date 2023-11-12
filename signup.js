72f4c92109cbef46d2ef085ed1626b28aa466469
chaebin <casevdev@gmail.com>
2023-11-13 00:43:45 +0900

    feat: "로그인폼 submit 핸들러 추가"

--- signup.js
+++ signup.js
@@ -2,7 +2,7 @@
 // v이메일 input에서 focus out 할 때, 이메일 형식에 맞지 않는 값이 있는 경우 input에 빨강색 테두리와 아래에 “올바른 이메일 주소가 아닙니다.” 빨강색 에러 메세지가 보입니다
 // v이메일 input에서 focus out 일 때, input 값이 test@codeit.com 일 경우 input에 빨강색 테두리와 아래에 “이미 사용 중인 이메일입니다.” 빨강색 에러 메세지가 보입니다.
 // v비밀번호 input에서 focus out 할 때, 값이 8자 미만으로 있거나 문자열만 있거나 숫자만 있는 경우, input에 빨강색 테두리와 아래에 “비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.” 빨강색 에러 메세지가 보입니다.
-// 비밀번호 input과 비밀번호 확인 input의 값이 다른 경우, 비밀번호 확인 input에 빨강색 테두리와 아래에 “비밀번호가 일치하지 않아요.” 빨강색 에러 메세지가 보입니다.
+// v비밀번호 input과 비밀번호 확인 input의 값이 다른 경우, 비밀번호 확인 input에 빨강색 테두리와 아래에 “비밀번호가 일치하지 않아요.” 빨강색 에러 메세지가 보입니다.
 // 회원가입을 실행할 경우, 문제가 있는 경우 문제가 있는 input에 빨강색 테두리와 에러 메세지가 보입니다.
 // 이외의 유효한 회원가입 시도의 경우, “/folder”로 이동합니다.
 // 이메일: test@codeit.com, 비밀번호: codeit101 으로 로그인 시도경우, “/folder” 페이지로 이동합니다.
@@ -79,7 +79,6 @@ function focusoutEmailInput(e){
     }
 };
 
-//위임 쓰지 말고 그냥 id. password 별로 달기 or form에 리스너 달고 핸들러에 if문 쓰기
 emailInputNode.addEventListener("focusout", focusoutEmailInput)
 passwordInputNode.addEventListener("focusout", focusoutPasswordInput)
 passwordCheckInputNode.addEventListener("input", validPasswordCheck);
