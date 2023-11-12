25e069733f54ef46cb8abe3602cb714f2e4d3300
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-11-12 15:46:00 +0900

    <refactor>: signup에서 checkPassword함수를 모듈화한 함수를 갖고와서 사용하는 checkPasswordEvent 함수로 수정

--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,5 @@
 import makeDOM from "./makeDOM.js"
-import { checkEmail, checkEmpty } from "./check.js";
+import { checkEmail, checkEmpty, checkPassword } from "./check.js";
 import showError from "./showError.js";
 import disappearError from "./disappearError.js";
 
@@ -31,16 +31,20 @@ const confirmPassword = (e) => {
   }
 }
 
-const checkPassword = (e) => {
-  if(e.target.id === 'password'){
-    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
-    if (passwordRegex.test(e.target.value)) return;
-    else {
-      if(e.target.value === '') return;
-      showError(e.target, '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.');
-    }
-  }
+const checkPasswordEvent = (e) => {
+  if(e.target.id === 'password') checkPassword(e.target);
 }
+
+// const checkPassword = (e) => {
+//   if(e.target.id === 'password'){
+//     const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
+//     if (passwordRegex.test(e.target.value)) return;
+//     else {
+//       if(e.target.value === '') return;
+//       showError(e.target, '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.');
+//     }
+//   }
+// }
 
 const testEmail = (e) => {
   if(e.target === emailInput) {
@@ -52,7 +56,7 @@ const testEmail = (e) => {
 
 emailInput.addEventListener('focusout', testEmail);
 passwordCheckInput.addEventListener('focusout', confirmPassword);
-passwordInput.addEventListener('focusout', checkPassword);
+passwordInput.addEventListener('focusout', checkPasswordEvent);
 emailInput.addEventListener('input', testEmail);
 passwordCheckInput.addEventListener('input', confirmPassword);
 passwordInput.addEventListener('input', checkPassword);
