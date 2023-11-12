ad2e335b4f0d5903644358c091985e50523a82a0
chaebin <casevdev@gmail.com>
2023-11-13 01:07:59 +0900

    feat: 회원가입폼 submit 핸들러 추가

--- signup.js
+++ signup.js
@@ -5,9 +5,9 @@
 // v비밀번호 input과 비밀번호 확인 input의 값이 다른 경우, 비밀번호 확인 input에 빨강색 테두리와 아래에 “비밀번호가 일치하지 않아요.” 빨강색 에러 메세지가 보입니다.
 // 회원가입을 실행할 경우, 문제가 있는 경우 문제가 있는 input에 빨강색 테두리와 에러 메세지가 보입니다.
 // 이외의 유효한 회원가입 시도의 경우, “/folder”로 이동합니다.
-// 이메일: test@codeit.com, 비밀번호: codeit101 으로 로그인 시도경우, “/folder” 페이지로 이동합니다.
+// v이메일: test@codeit.com, 비밀번호: codeit101 으로 로그인 시도경우, “/folder” 페이지로 이동합니다.
 // v비밀번호 input에서 focus out 할 때, 값이 없을 경우 아래에 “비밀번호를 입력해주세요.” 에러 메세지가 보입니다.
-// 이외의 로그인 시도의 경우 이메일, 비밀번호 input에 빨강색 테두리와 각각의 input아래에 “이메일을 확인해주세요.”, “비밀번호를 확인해주세요.” 빨강색 에러 메세지가 보입니다.
+// v이외의 로그인 시도의 경우 이메일, 비밀번호 input에 빨강색 테두리와 각각의 input아래에 “이메일을 확인해주세요.”, “비밀번호를 확인해주세요.” 빨강색 에러 메세지가 보입니다.
 // 회원가입 버튼 클릭 또는 Enter키 입력으로 회원가입 됩니다.
 // v이메일, 비밀번호 input에 에러 관련 디자인을 Components 영역의 에러 케이스로 적용합니다.
 const CLASSNAME_ERROR = "error";
@@ -17,6 +17,22 @@ const CLASSNAME_BLIND = "blind";
 const emailInputNode = document.querySelector(".signForm_input.email")
 const passwordInputNode = document.querySelectorAll(".signForm_input.password")[0]
 const passwordCheckInputNode = document.querySelectorAll(".signForm_input.password")[1]
+
+const signForm = document.querySelector(".signForm")
+function handleSubmitSignIn(e){
+    e.preventDefault()
+    const email = e.target.email.value
+    const password = e.target.password.value
+    if (email === "test@codeit.com" && password ==="codeit101"){
+        window.location.assign("./folder.html")
+    }
+    else {
+        removeErrorClass(emailInputNode)
+        removeErrorClass(passwordInputNode)
+        addErrorClass(emailInputNode, "이메일을 확인해주세요")
+        addErrorClass(passwordInputNode, "비밀번호를 확인해주세요")
+    }
+}
 
 function validPasswordCheck(e){
     const password = passwordInputNode.value
