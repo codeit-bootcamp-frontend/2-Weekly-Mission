ce7a9c3c4f9ea59bc618626ccd3003f66ccb88be
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-11-12 15:14:28 +0900

    <refactor>: signin.js에서 email 형식을 체크하는 함수 checkEmail을 emailCheckEvent로 이름을 변경하고 함수 내용은 emailCheck함수 모듈을 갖고와서 사용

--- js/signin.js
+++ js/signin.js
@@ -1,26 +1,32 @@
+import { emailCheck } from "./check.js";
 import makeDOM from "./makeDOM.js";
 import showError from "./showError.js";
 
 const emailInput = document.querySelector('#email');
 const signInBtn = document.querySelector('.signin--btn');
 
-const checkEmail = (e) => {
-  if(e.target === emailInput) {
-    if(e.target.value === '') return;
-    const emailValue = e.target.value.trim();
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if (emailRegex.test(emailValue)) {
-      if(e.target.parentNode.lastChild.classList.contains('error-message')) {
-        emailInput.parentNode.removeChild(emailInput.parentNode.lastChild);
-        emailInput.classList.remove('input--error')
-      }
-      else return;
-    }
-    else {
-      showError(emailInput, '올바른 이메일 주소가 아닙니다.');
-    }
-  }
-};
+// const checkEmail = (e) => {
+//   if(e.target === emailInput) {
+//     if(e.target.value === '') return;
+//     const emailValue = e.target.value.trim();
+//     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+//     if (emailRegex.test(emailValue)) {
+//       if(e.target.parentNode.lastChild.classList.contains('error-message')) {
+//         emailInput.parentNode.removeChild(emailInput.parentNode.lastChild);
+//         emailInput.classList.remove('input--error')
+//       }
+//       else return;
+//     }
+//     else {
+//       showError(emailInput, '올바른 이메일 주소가 아닙니다.');
+//     }
+//   }
+// };
+
+const emailCheckEvent = (e) => {
+  if(e.target === document.querySelector('#email')) emailCheck(e.target);
+}
+
 const signInTest = (e) => {
   e.preventDefault();
   if(e.target === document.querySelector('.signin--btn')){
@@ -39,6 +45,6 @@ const signInTest = (e) => {
   }
 };
 
-emailInput.addEventListener('input', checkEmail);
-emailInput.addEventListener('blur', checkEmail);
+emailInput.addEventListener('input', emailCheckEvent);
+emailInput.addEventListener('blur', emailCheckEvent);
 signInBtn.addEventListener('click', signInTest);
