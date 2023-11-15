e05104fa4f1f8c04aae40800aada6151eb082ddb
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-11-15 16:29:32 +0900

    Refactor: 로그인 시도 함수를 authVerifyUser로 이동

--- js/modules/authVerifyUser.js
+++ js/modules/authVerifyUser.js
@@ -1,19 +1,35 @@
 import userData from "../database/userData.js";
+import { $emailInput, $passwordInput } from "./domElements.js";
+import { triggerInputValidationError } from "./authDOMHandler.js";
 
-const verifyLoginCredentials = (email, password) => {
-  for (const key in userData) {
-    if (userData[key].email === email) {
-      if (userData[key].password === password) {
-        return true;
-      } else {
-        return false;
-      }
-    } else {
-      return false;
+const API_URL = "https://bootcamp-api.codeit.kr";
+
+const postLogin = async (user) => {
+  try {
+    const response = await fetch(`${API_URL}/api/sign-in`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    if (!response.ok) {
+      throw new Error();
     }
+    const result = await response.json();
+    const { data } = result;
+
+    for (const key in data) {
+      localStorage.setItem(key, data[key]);
+    }
+
+    location.href = "./folder.html";
+  } catch (error) {
+    triggerInputValidationError($emailInput, "이메일을 확인해주세요");
+    triggerInputValidationError($passwordInput, "비밀번호를 확인해주세요.");
   }
 };
 
 const checkEmailExist = (email) => Object.values(userData).some((user) => user.email === email);
 
-export { verifyLoginCredentials, checkEmailExist };
+export { postLogin, checkEmailExist };
--- js/signin.js
+++ js/signin.js
@@ -1,7 +1,6 @@
 import { $submitBtn, $emailInput, $passwordInput } from "./modules/domElements.js";
 import { initializeSignForm as initializeSignInForm } from "./modules/authEventHandler.js";
-import { triggerInputValidationError } from "./modules/authDOMHandler.js";
-import { verifyLoginCredentials } from "./modules/authVerifyUser.js";
+import { postLogin } from "./modules/authVerifyUser.js";
 
 const redirectIfAccessTokenExists = (directURL) => {
   const accessToken = localStorage.getItem("accessToken");
@@ -13,37 +12,16 @@ const redirectIfAccessTokenExists = (directURL) => {
 redirectIfAccessTokenExists("./folder.html");
 
 initializeSignInForm();
+
 const submitSignInHandler = $submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
+
+  // user 객체 생성
   const user = {
     email: $emailInput.value,
     password: $passwordInput.value,
   };
-  tryLogin(user);
+
+  // login 시도
+  postLogin(user);
 });
-
-const tryLogin = async (user) => {
-  try {
-    const response = await fetch("https://bootcamp-api.codeit.kr/api/sign-in", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
-    });
-    if (!response.ok) {
-      throw new Error();
-    }
-    const result = await response.json();
-    const { data } = result;
-
-    for (const key in data) {
-      localStorage.setItem(key, data[key]);
-    }
-
-    location.href = "./folder.html";
-  } catch (error) {
-    triggerInputValidationError($emailInput, "이메일을 확인해주세요");
-    triggerInputValidationError($passwordInput, "비밀번호를 확인해주세요.");
-  }
-};
