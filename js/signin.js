f12b817dd2cf0ed03a930fb8bd245ef910ce437e
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-11-15 16:09:42 +0900

    Feature: 올바른 로그인 시도시 로컬스토리지에 accessToken 저장하도록 하는 기능 개발
    - 저장 후 folder.html로 이동
    - 만약 로컬스토리지에 accessToken이 존재할 경우 로그인 페이지 이동시 folder로 redirect 되도록 함수 개발

--- js/signin.js
+++ js/signin.js
@@ -3,8 +3,16 @@ import { initializeSignForm as initializeSignInForm } from "./modules/authEventH
 import { triggerInputValidationError } from "./modules/authDOMHandler.js";
 import { verifyLoginCredentials } from "./modules/authVerifyUser.js";
 
-initializeSignInForm();
+const redirectIfAccessTokenExists = (directURL) => {
+  const accessToken = localStorage.getItem("accessToken");
+  if (accessToken) {
+    location.replace(directURL);
+  }
+};
 
+redirectIfAccessTokenExists("./folder.html");
+
+initializeSignInForm();
 const submitSignInHandler = $submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
   const user = {
@@ -13,11 +21,6 @@ const submitSignInHandler = $submitBtn.addEventListener("click", (e) => {
   };
   tryLogin(user);
 });
-// if (verifyLoginCredentials($emailInput.value, $passwordInput.value)) {
-//   $submitBtn.parentElement.action = "./folder.html";
-// } else {
-
-// }
 
 const tryLogin = async (user) => {
   try {
@@ -31,7 +34,14 @@ const tryLogin = async (user) => {
     if (!response.ok) {
       throw new Error();
     }
-    // console.log(response);
+    const result = await response.json();
+    const { data } = result;
+
+    for (const key in data) {
+      localStorage.setItem(key, data[key]);
+    }
+
+    location.href = "./folder.html";
   } catch (error) {
     triggerInputValidationError($emailInput, "이메일을 확인해주세요");
     triggerInputValidationError($passwordInput, "비밀번호를 확인해주세요.");
