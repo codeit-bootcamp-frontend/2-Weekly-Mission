5d34a8ae4c920e48ef86b5e40e09ff4a16c919c3
chaebin <casevdev@gmail.com>
2023-11-16 04:05:06 +0900

    Refactor: togglePasswordHidden 함수의 마크업 구조로부터 독립성을 향상

--- scripts/modules/toggleBtn.js
+++ scripts/modules/toggleBtn.js
@@ -1,5 +1,6 @@
 function togglePasswordHidden(e){
-    const passwordInput = e.target.parentElement.firstElementChild
+    const parentLabel = e.target.closest(".signForm_label");
+    const passwordInput = parentLabel.querySelector(".signForm_input");
     e.preventDefault()
     passwordInput.type = passwordInput.type === "text" ? "password" : "text" 
 
