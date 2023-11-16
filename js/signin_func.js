574a42d64e8cc6cda89ab9bce04971f44e0f3822
yunsusu <rladbstn1212@gmail.com>
2023-11-16 19:20:06 +0900

    sdaf

--- js/signin_func.js
+++ js/signin_func.js
@@ -4,33 +4,33 @@ function login_wrong_pwd(){
     let mailWarning = mail.nextElementSibling;
     let pwdWarning = eye.nextElementSibling;
     pwd.style.borderColor = "#ff5b56";
-    let pwd_noligin = setmessage("비밀번호를 확인해주세요.")
+    let pwd_nologin = setmessage("비밀번호를 확인해주세요.")
     if(pwdWarning || mailWarning){
-        pwd.parentElement.replaceChild(pwd_noligin, pwdWarning);
+        pwd.parentElement.replaceChild(pwd_nologin, pwdWarning);
     }else if(pwdWarning && !mailWarning){
-        pwd.parentElement.replaceChild(pwd_noligin, pwdWarning);
+        pwd.parentElement.replaceChild(pwd_nologin, pwdWarning);
     }else if(!pwdWarning && mailWarning){
-        pwd.parentElement.append(pwd_noligin);
+        pwd.parentElement.append(pwd_nologin);
     }else{
-        pwd.parentElement.append(pwd_noligin);
+        pwd.parentElement.append(pwd_nologin);
     }
 }
 
 function login_wrong_mail(){
     let mailWarning = mail.nextElementSibling;
-    let mail_noligin = setmessage("이메일을 확인해주세요.");
+    let mail_nologin = setmessage("이메일을 확인해주세요.");
     mail.style.borderColor = "#ff5b56";
     if(pwdWarning || mailWarning){
-        mail.parentElement.replaceChild(mail_noligin, mailWarning);
+        mail.parentElement.replaceChild(mail_nologin, mailWarning);
     }else if(pwdWarning && !mailWarning){
-        mail.parentElement.append(mail_noligin);
+        mail.parentElement.append(mail_nologin);
     }else if(!pwdWarning && mailWarning){
-        mail.parentElement.replaceChild(mail_noligin, mailWarning);
+        mail.parentElement.replaceChild(mail_nologin, mailWarning);
     }else{
-        mail.parentElement.append(mail_noligin);
+        mail.parentElement.append(mail_nologin);
     }
 }
-// import
+
 function eye_toggle(){
     for(let i = 0; i<eye.children.length; i++){
         eye.children[i].classList.toggle("eye_none");
@@ -56,18 +56,34 @@ function mail_focusout(a){
         mail.style.borderColor = "#ccd5e3";
     }
 }
-function login_go(e){
-    if(mail.value === "test@codeit.com" && pwd.value === "codeit101"){
-        console.log('login~')
-    }else if(mail.value === "test@codeit.com" && pwd.value !== "codeit101"){
-        e.preventDefault()
-        login_wrong_pwd()
-    }else if(mail.value !== "test@codeit.com" && pwd.value !== "codeit101"){
-        e.preventDefault()
-        login_wrong_pwd()
-        login_wrong_mail()
+function login_go(e){e.preventDefault()
+    const logining = {
+        email:"test@codeit.com",
+        password : "sprint101",
     }
-}function pwd_focusout(){
+    fetch("https://bootcamp-api.codeit.kr/api/sign-in",{
+        method : 'POST',
+        body : JSON.stringify(logining),
+    })
+        .then((response)=> response.json())
+        .then((result)=> {console.log(result)
+            if(mail.value === "test@codeit.com" && pwd.value === "codeit101"){
+                console.log('login~')
+            }else if(mail.value === "test@codeit.com" && pwd.value !== "codeit101"){
+                e.preventDefault()
+                login_wrong_pwd()
+            }else if(mail.value !== "test@codeit.com" && pwd.value !== "codeit101"){
+                e.preventDefault()
+                login_wrong_pwd()
+                login_wrong_mail()
+            }
+        })
+        .catch((error)=>{console.log(error)})
+        .finally(()=>{console.log("asdf")})
+
+    
+}
+function pwd_focusout(){
     let pwdWarning = eye.nextElementSibling;
     let pwd_null = setmessage("비밀번호를 입력해주세요.")
     if(pwd.value === ""){
