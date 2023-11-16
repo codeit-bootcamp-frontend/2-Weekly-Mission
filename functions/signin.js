f3043a761b7674e72f222985d23147f2537ed3e4
Jisoo Han <llqrw55@naver.com>
2023-11-16 21:38:04 +0900

    refactor:회원가입 리팩터링

--- functions/signin.js
+++ functions/signin.js
@@ -1,104 +1,88 @@
-import{emailRegex,passwordRegex,updateAlert,removeAlert,} from "./utils.js"
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
-const inputEmail = document.querySelector('#email__input');
-const inputPassword = document.querySelector('#password__input');
-const loginButton = document.querySelector('#login-page-button')
-const passwordIcon = document.querySelector('.password-icon')
+import { message, emailRegex, passwordRegex, updateAlert, removeAlert } from "./utils.js";
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const inputEmail = document.querySelector("#email__input");
+const inputPassword = document.querySelector("#password__input");
+const loginButton = document.querySelector("#login-page-button");
+const passwordIcon = document.querySelector(".password-icon");
 
 
 
-
-export let message={
-    email:{
-        null:"이메일을 입력해주세요",
-        invalid:"올바른 이메일 주소가 아닙니다.",
-        duplicate:"이미 사용중인 이메일입니다.",
-        check:"이메일을 확인해주세요."
-    },
-    password:{
-        null:"비밀번호를 입력해주세요",
-        invalid:"비밀번호는 영문,숫자 조합 8자 이상 입력해 주세요.",
-        match:"비밀번호가 일치하지 않아요.",
-        check:"비밀번호를 확인해주세요."
+//객체의 키로 줄여보기
+function validation(choice) {
+  let result = "";
+  if (choice == email) {
+    if (inputEmail.value === "") {
+      result = message.email.null;
+    } else if (!emailRegex.test(inputEmail.value)) {
+      result = message.email.invalid;
+    } else {
+      result = "";
     }
+  } else if (choice == password) {
+    if (inputPassword.value === "") {
+      result = message.password.null;
+    } else if (!passwordRegex.test(inputPassword.value)) {
+      result = message.password.invalid;
+    } else {
+      result = "";
+    }
+  }
+  return result;
 }
 
-function validation(choice){
-    let result = "" 
-    if(choice == email){
-        if(inputEmail.value === ""){
-            result=  message.email.null
-        }else if(!emailRegex.test(inputEmail.value)){
-            result = message.email.invalid
-        }else{
-            result = ""
-        }
-    }else if(choice == password){
-        if(inputPassword.value === ""){
-            result=  message.password.null
-        }else if(!passwordRegex.test(inputPassword.value)){
-            result = message.password.invalid
-        }else{
-            result = ""
-        }
-    }
-    return result
+function emailFocus(e) {
+  let alert = document.querySelector(".email-alert-text");
+  if (alert) {
+    alert.remove();
+  }
+  if (validation(e.c) === "") {
+    removeAlert(alert, inputEmail);
+  } else {
+    updateAlert(inputEmail, email, "email-alert-text", validation(email));
+  }
 }
 
-
-function emailFocus(){
-    let alert = document.querySelector('.email-alert-text')
-    if(alert){
-        alert.remove();
-    }
-    if(validation(email) === ""){
-        removeAlert(alert,inputEmail)
-    }else{
-        updateAlert(inputEmail, email,"email-alert-text", validation(email));
-    }       
-}
-
-
-function passwordFocus(e){
-    let alert = document.querySelector('.password-alert-text')
-    if(alert){
-        alert.remove();
-    }
-    if(validation(password) === ""){
-        removeAlert(alert,inputPassword)
-    }else{
-        updateAlert(inputPassword, password,"password-alert-text", validation(password));
-    }       
+function passwordFocus(e) {
+  let alert = document.querySelector(".password-alert-text");
+  if (alert) {
+    alert.remove();
+  }
+  if (validation(password) === "") {
+    removeAlert(alert, inputPassword);
+  } else {
+    updateAlert(inputPassword, password, "password-alert-text", validation(password));
+  }
 }
 
 function login(e) {
-    e.preventDefault();
-    if (validation(email)=="" && validation(password)=="") {
-        window.location.href = "../folder.html";
-    }else{
-        if(validation(email) !==""){
-           emailFocus();
-        }
-        if(validation(password) !==""){
-            passwordFocus();
-         }
+  e.preventDefault();
+  if (validation(email) == "" && validation(password) == "") {
+    window.location.href = "../folder.html";
+  } else {
+    if (validation(email) !== "") {
+      emailFocus();
     }
+    if (validation(password) !== "") {
+      passwordFocus();
+    }
+  }
 }
 
-let passwordToggle =false;
+// 전역변수 고려해서 은닉화 하기
+let passwordToggle = false;
 function passwordActivation() {
-    passwordToggle = !passwordToggle
-    if (!passwordToggle) {
-        passwordIcon.setAttribute('src', '../images/password-icon.png');
-        inputPassword.type = 'password';
-    } else {
-        passwordIcon.setAttribute('src', '../images/eye-on.png');
-        inputPassword.type = 'text';
-    }
+  passwordToggle = !passwordToggle;
+  if (!passwordToggle) {
+    passwordIcon.setAttribute("src", "../images/password-icon.png");
+    inputPassword.type = "password";
+  } else {
+    passwordIcon.setAttribute("src", "../images/eye-on.png");
+    inputPassword.type = "text";
+  }
 }
 
-inputEmail.addEventListener('focusout', emailFocus);
-inputPassword.addEventListener('focusout', passwordFocus);
-loginButton?.addEventListener('click', login);
-passwordIcon.addEventListener('click',passwordActivation);
+inputEmail.addEventListener("focusout", emailFocus);
+inputPassword.addEventListener("focusout", passwordFocus);
+loginButton.addEventListener("click", login);
+passwordIcon.addEventListener("click", passwordActivation);
--- functions/signup.js
+++ functions/signup.js
@@ -1,120 +1,121 @@
-import {message} from "./signin.js"
-import{emailRegex,passwordRegex,updateAlert,removeAlert,} from "./utils.js"
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
-const passwordCheck = document.querySelector('#password-check');
-const inputEmail = document.querySelector('#email__input');
-const inputPassword = document.querySelector('#password__input');
-const inputPasswordCheck = document.querySelector('#password-check__input');
-const joinButton = document.querySelector('#join-page-button');
-const passwordIcons = document.querySelectorAll('.password-icon');
+import { message, emailRegex, passwordRegex, updateAlert, removeAlert } from "./utils.js";
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const passwordCheck = document.querySelector("#password-check");
+const inputEmail = document.querySelector("#email__input");
+const inputPassword = document.querySelector("#password__input");
+const inputPasswordCheck = document.querySelector("#password-check__input");
+const joinButton = document.querySelector("#join-page-button");
+const passwordIcons = document.querySelectorAll(".password-icon");
 
-
-
-function validation(choice){
-    let result = "" 
-    if(choice == email){
-        if(inputEmail.value === ""){
-            result=  message.email.null
-        }else if(!emailRegex.test(inputEmail.value)){
-            result = message.email.invalid
-        }else if(inputEmail.value === "test@codeit.com"){
-            result = message.email.duplicate
-        }else{
-            result = ""
-        }
-    }else if(choice == password){
-        if(inputPassword.value === ""){
-            result=  message.password.null
-        }else if(!passwordRegex.test(inputPassword.value)){
-            result = message.password.invalid
-        }else{
-            result = ""
-        }
-    }else if(choice == passwordCheck){
-        if(inputPasswordCheck.value==""){
-            result = message.password.check
-        }else if(inputPassword.value !== inputPasswordCheck.value){
-            result = message.password.match
-        }else{
-            result = ""
-        }
-    }
-    return result
+function validateEmail(selectInput) {
+  let inputValue = selectInput;
+  if (inputValue == "") {
+    return message.email.null;
+  }else if(!emailRegex.test(inputValue)){
+    return message.email.invalid;
+  }else if(emailValue === "test@codeit.com"){
+    return message.email.duplicate;
+  }else{
+    return ""
+  }
 }
 
-
-function emailFocus(){
-    let alert = document.querySelector('.email-alert-text')
-    if(alert){
-        alert.remove();
-    }
-    if(validation(email) === ""){
-        removeAlert(alert,inputEmail)
-    }else{
-        updateAlert(inputEmail, email,"email-alert-text", validation(email));
-    }       
+function validatePassword(selectInput){
+  let inputValue = selectInput
+  if (inputValue == "") {
+    return message.password.null;
+  }else if(!passwordRegex.test(inputValue)){
+    return message.password.invalid;
+  }else{
+    return ""
+  }
 }
 
-function passwordFocus(){
-    let alert = document.querySelector('.password-alert-text')
-    if(alert){
-        alert.remove();
-    }
-    if(validation(password) === ""){
-        removeAlert(alert,inputPassword)
-    }else{
-        updateAlert(inputPassword, password,"password-alert-text", validation(password));
-    }       
+function validatePasswordCheck(selectInput){
+  let inputValue = selectInput
+  if (inputValue == "") {
+    return message.password.check;
+  }else if(inputPassword.value != inputValue){
+    return message.password.match;
+  }
+    return ""
 }
 
-function passwordCheckFocus(){
-    let alert = document.querySelector('.check-alert-text')
-    if(alert){
-        alert.remove();
-    }
-    if(validation(passwordCheck) === ""){
-        removeAlert(alert,inputPasswordCheck)
-    }else{
-        updateAlert(inputPasswordCheck, passwordCheck,"check-alert-text", validation(passwordCheck));
-    }   
+function emailFocus(e) {
+  const errorMessage = validateEmail(e.target.value);
+  let alert = document.querySelector(".email-alert-text");
+  if (alert) {
+    alert.remove();
+  }
+  if (errorMessage === "") {
+    removeAlert(alert, inputEmail);
+  } else {
+    updateAlert(inputEmail, email, "email-alert-text", errorMessage);
+  }
 }
 
+function passwordFocus(e) {
+  const errorMessage = validatePassword(e.target.value);
+  let alert = document.querySelector(".password-alert-text");
+  if (alert) {
+    alert.remove();
+  }
+  if (errorMessage === "") {
+    removeAlert(alert, inputPassword);
+  } else {
+    updateAlert(inputPassword, password, "password-alert-text", errorMessage);
+  }
+}
+
+function passwordCheckFocus(e) {
+  const errorMessage = validatePasswordCheck(e.target.value);
+  let alert = document.querySelector(".check-alert-text");
+  if (alert) {
+    alert.remove();
+  }
+  if (errorMessage === "") {
+    removeAlert(alert, inputPasswordCheck);
+  } else {
+    updateAlert(inputPasswordCheck, passwordCheck, "check-alert-text", errorMessage);
+  }
+}
 
 function join(e) {
-    e.preventDefault();
-    if (validation(email)=="" && validation(password)=="" && validation(passwordCheck) =="") {
-        window.location.href = "../folder.html";
-    }else{
-        if(validation(email) !==""){
-           emailFocus();
-        }
-        if(validation(password) !==""){
-            passwordFocus();
-         }
-         if(validation(passwordCheck) !==""){
-            passwordCheckFocus();
-         }
+  e.preventDefault();
+  if (validateEmail == "" && validatePassword == "" && validatePasswordCheck == "") {
+    window.location.href = "../folder.html";
+  } else {
+    if (inputEmail !== "") {
+      emailFocus();
     }
+    if (inputPassword !== "") {
+      passwordFocus();
+    }
+    if (inputPasswordCheck !== ""){
+      passwordCheckFocus();
+    }
+  }
 }
 
-
-let passwordToggle =false;
+let passwordToggle =false
 function passwordActivation(e) {
-    passwordToggle = !passwordToggle
-    if (!passwordToggle) {
-        e.target.setAttribute('src', '../images/password-icon.png');
-        inputPassword.type = 'password';
-    } else {
-        e.target.setAttribute('src', '../images/eye-on.png');
-        inputPassword.type = 'text';
-    }
+  let input = e.target.parentElement
+  passwordToggle = !passwordToggle
+  console.log(input)
+  if (!passwordToggle) {
+    e.target.setAttribute("src", "../images/password-icon.png");
+    input.type = "password";
+  } else {
+    e.target.setAttribute("src", "../images/eye-on.png");
+    input.type = "text";
+  }
 }
 
-inputEmail.addEventListener('focusout', emailFocus);
-inputPassword.addEventListener('focusout', passwordFocus);
-inputPasswordCheck.addEventListener('focusout', passwordCheckFocus);
-passwordIcons.forEach((icon)=>{
-    icon.addEventListener('click' ,passwordActivation)
-})
-joinButton.addEventListener('click', join );
+inputEmail.addEventListener("focusout", emailFocus);
+inputPassword.addEventListener("focusout", passwordFocus);
+inputPasswordCheck.addEventListener("focusout", passwordCheckFocus);
+passwordIcons.forEach((icon) => {
+  icon.addEventListener("click", passwordActivation);
+});
+joinButton.addEventListener("click", join);
