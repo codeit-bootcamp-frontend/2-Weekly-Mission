70347cb1d85432420520d2d8592aea96ad326678
김우현 <rladngus133@naver.com>
2023-11-19 04:16:38 +0900

    feat: 서버에 이메일 존재시 이미 사용 중인 이메일 경고노출

--- script/module/signForm/btnEvent.mjs
+++ script/module/signForm/btnEvent.mjs
@@ -73,6 +73,7 @@ async function signUpBtnHandler(e) {
     );
 
     if (emailCkdResponse.status === 409) {
+      showError(pwCheckErrorMsg, pwCheckInput, "이미 사용 중인 이메일입니다.");
       return;
     }
 
