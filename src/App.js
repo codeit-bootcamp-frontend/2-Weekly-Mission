cf141b1bb480706e68733bce08eefb9b97e69bc5
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-11-24 17:48:41 +0900

    <feat>: 내비게이션바에 프로필 추가
    api를 통해 user를 성공적으로 받아올 경우 프로필을 보여주고 user를 못받아오거나 없을 경우 로그인 버튼 보여주는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import axios from 'axios';
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
+import { API_URL } from './constants/constants';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState({});
+
+  const getUser = async() => {
+    try{
+      const response = await axios.get(`${API_URL}/user`);
+      setUser(response.data);
+      console.log(response.data);
+    } catch(error) {
+      console.log(error);
+      setUser(null);
+    }
+  }
+
+  useEffect(() => {
+    getUser();
+  },[])
+
   return (
     <div className="App">
-      <Nav/>
+      <Nav profile={user}/>
       <Footer/>
     </div>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,24 @@
-import logo from '../assets/logo.svg'
+import logo from '../assets/logo.svg';
 import '../styles/Nav.css'
 
-const Nav = () => {
+const UserProfile = ({profile}) => {
+  return(
+    <div className='profile--box'>
+      <img src={profile.profileImageSource} alt='프로필 아이콘' className='profile-image'/>
+      <p className='profile-email'>{profile.email}</p>
+    </div>
+  )
+};
+
+const Nav = ({profile}) => {
   return(
     <header className="landing--header">
       <div className="header-bar">
         <a href="/" className="landing--logo">
           <img src={logo} alt="logo"/>
         </a>
-        <a href="./signin.html" className="login--btn btn">로그인</a>
+        {profile && <UserProfile profile={profile}></UserProfile>}
+        {(profile === null) && <a href="./signin.html" className="login--btn btn">로그인</a>}
       </div>
     </header>
   )
