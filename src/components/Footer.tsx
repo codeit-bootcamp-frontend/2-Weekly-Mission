f145fb2991846e77bba79ef0ee4ab735f9dc4406
KimByeongHwa <iambyeonghwa@gmail.com>
2023-11-25 18:58:08 +0900

    feat: Footer UI 완성

---
+++ src/components/Footer.tsx
@@ -0,0 +1,81 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.black};
+  width: 100%;
+  height: 160px;
+`;
+
+const Container = styled.div`
+  font-family: 'Abel';
+  font-size: 1.6rem;
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.gray100};
+  max-width: 1232px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 3.2rem;
+`;
+
+const CopyRight = styled.div``;
+
+const NavLinks = styled.div``;
+
+const NavLink = styled.a`
+  cursor: pointer;
+
+  & + & {
+    margin-left: 3rem;
+  }
+`;
+
+const SNS = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SNSLogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 2rem;
+  height: 2rem;
+  cursor: pointer;
+
+  & + & {
+    margin-left: 1rem;
+  }
+`;
+
+function Footer() {
+  return (
+    <Wrapper>
+      <Container>
+        <CopyRight>©codeit - 2023</CopyRight>
+        <NavLinks>
+          <NavLink>Privacy Policy</NavLink>
+          <NavLink>FAQ</NavLink>
+        </NavLinks>
+        <SNS>
+          <SNSLogoWrapper>
+            <img src='/images/facebook-icon.png' alt='facebook-icon' />
+          </SNSLogoWrapper>
+          <SNSLogoWrapper>
+            <img src='/images/twitter-icon.png' alt='twitter-icon' />
+          </SNSLogoWrapper>
+          <SNSLogoWrapper>
+            <img src='/images/youtube-icon.png' alt='youtube-icon' />
+          </SNSLogoWrapper>
+          <SNSLogoWrapper>
+            <img src='/images/instagram-icon.png' alt='instagram-icon' />
+          </SNSLogoWrapper>
+        </SNS>
+      </Container>
+    </Wrapper>
+  );
+}
+
+export default Footer;
