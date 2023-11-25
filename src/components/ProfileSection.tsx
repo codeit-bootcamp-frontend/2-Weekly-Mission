060324ddb493b31e3fee35972f5386385d603a35
KimByeongHwa <iambyeonghwa@gmail.com>
2023-11-26 05:28:03 +0900

    feat: 상단 프로필 영역 구현

---
+++ src/components/ProfileSection.tsx
@@ -0,0 +1,95 @@
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import TopBar from './TopBar';
+
+const Container = styled.div`
+	width: 100%;
+	height: 33.7rem;
+	background-color: ${({ theme }) => theme.colors.skyblue};
+`;
+
+const ContentsWrapper = styled.div`
+	width: fit-content;
+	margin: 2rem auto 0;
+`;
+
+const UserWrapper = styled.div`
+	text-align: center;
+	margin-bottom: 2rem;
+`;
+
+const ImgWrapper = styled.div`
+	width: 6rem;
+	height: 6rem;
+	border-radius: 70%;
+	overflow: hidden;
+	margin: 0 auto 1.2rem;
+`;
+
+const UserProfileImg = styled.img`
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+`;
+
+const UserName = styled.div`
+	font-family: 'Pretendard';
+	font-size: 1.6rem;
+`;
+
+const FolderName = styled.div`
+	font-family: 'Pretendard';
+	font-size: 4rem;
+	font-weight: 600;
+	text-align: center;
+`;
+
+interface User {
+	id: number;
+	name: string;
+	email: string;
+	profileImageSource: string;
+}
+
+function ProfileSection() {
+	const [user, setUser] = useState<User | null>(null);
+
+	const getUser = async () => {
+		try {
+			const res = await fetch('https://bootcamp-api.codeit.kr/api/sample/user');
+			const data = await res.json();
+			return data;
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		getUser().then((data) => {
+			setUser(data);
+		});
+	}, []);
+	// console.log(user);
+
+	const userId = user?.id;
+	const userName = user?.name;
+	const userEmail = user?.email;
+	const userProfileImg = user?.profileImageSource;
+
+	return (
+		<Container>
+			<TopBar email={userEmail} profileImageSource={userProfileImg} />
+			<ContentsWrapper>
+				<UserWrapper>
+					<ImgWrapper>
+						<UserProfileImg src={userProfileImg} alt='profile-img' />
+					</ImgWrapper>
+					<UserName>{userName}</UserName>
+				</UserWrapper>
+				<FolderName>⭐️ 즐겨찾기</FolderName>
+			</ContentsWrapper>
+		</Container>
+	);
+}
+
+export default ProfileSection;
