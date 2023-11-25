693935a7afefd3b4557cfb4a81e4d66c964f5265
박윤수 <ys1008p@gmail.com>
2023-11-25 22:08:06 +0900

    feat: 리액트로 header, footer 구현

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { App } from "./App.js";
+import "./css/style.css";
+import "./css/common/common.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<App />);
