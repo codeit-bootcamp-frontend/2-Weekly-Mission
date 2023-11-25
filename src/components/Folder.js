b8e96fd6e59b7bbbadaf8ba823d1b06e3fddadef
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-11-25 15:39:59 +0900

    <feat>: Folder 컴포넌트 추가
    Folder Owner의 정보와 search bar, folder의 링크들을 모아놓은 컴포넌트 Folder 생성 및 추가
    해당 Folder는 props로 owner의 정보와 folder의 이름, folder에 있는 links의 배열을 받음
    FolderOwner, SearchBar, Card 컴포넌트로 구성

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -1,13 +1,16 @@
 import Card from "./Card";
 import FolderOwner from "./FolderOwner";
 import SearchBar from "./SearchBar";
+import '../styles/Folder.css';
 
 const Folder = ({ owner, name, links }) => {
   return(
     <main className="contents folder-link--main">
       <FolderOwner owner={owner} name={name}></FolderOwner>
       <SearchBar></SearchBar>
-      {links.map((link) => <Card link={link}></Card>)}
+      <section className="card--section">
+        {links.map((link) => <Card link={link}></Card>)}
+      </section>
     </main>
   )
 };
