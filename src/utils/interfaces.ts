b66cab38f73a2c0dc62dcd3efa03038086c8366b
KimByeongHwa <iambyeonghwa@gmail.com>
2023-11-27 03:26:49 +0900

    refactor: interface 모듈화

---
+++ src/utils/interfaces.ts
@@ -0,0 +1,42 @@
+// GradationButton.tsx
+export interface GradationButtonProps {
+	children: string;
+	width?: string;
+}
+
+// Card.tsx
+export interface CardProps {
+	link: Link;
+}
+
+// ProfileSection.tsx
+export interface User {
+	id: number;
+	name: string;
+	email: string;
+	profileImageSource: string;
+}
+
+// ListSection.tsx
+export interface Link {
+	createdAt: string;
+	description: string;
+	id: number;
+	imageSource: string;
+	title: string;
+	url: string;
+}
+
+export interface Owner {
+	id: number;
+	name: string;
+	profileImageSource: string;
+}
+
+export interface Folder {
+	count: number;
+	id: number;
+	links: Link[];
+	name: string;
+	owner: Owner;
+}
