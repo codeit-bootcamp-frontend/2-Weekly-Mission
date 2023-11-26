76442215a66abac35175ce66fa792dcec4b1e631
KimByeongHwa <iambyeonghwa@gmail.com>
2023-11-27 07:08:59 +0900

    feat: 하단 카드 영역 구현

--- src/components/layouts/ListSection.tsx
+++ src/components/layouts/ListSection.tsx
@@ -4,13 +4,51 @@ import Card from '../molecules/Card';
 import { Folder } from '../../utils/interfaces';
 
 const Container = styled.div`
-	width: fit-content;
+	width: 80%;
 	margin: 0 auto;
+	padding: 4rem 0;
 `;
 
-const SearchBar = styled.div``;
+const SearchWrapper = styled.div`
+	width: 100%;
+	height: 5.4rem;
+	border-radius: 10px;
+	background-color: #f5f5f5;
+	position: relative;
+	display: flex;
+	align-items: center;
+	padding: 1.5rem 1.6rem;
+`;
 
-const CardContainer = styled.div``;
+const SearchBar = styled.input`
+	background-color: #f5f5f5;
+	border: none;
+	outline: none;
+	font-size: 1.6rem;
+	margin-left: 2.6rem;
+`;
+
+const SearchIcon = styled.div`
+	width: 1.6rem;
+	height: 1.6rem;
+	position: absolute;
+	cursor: pointer;
+
+	img {
+		width: 100%;
+		height: 100%;
+		text-align: center;
+	}
+`;
+
+const CardContainer = styled.div`
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	grid-gap: 2rem;
+	justify-items: center;
+	align-items: center;
+	padding: 4rem 0;
+`;
 
 function ListSection() {
 	const [folder, setFolder] = useState<Folder | null>(null);
@@ -24,7 +62,6 @@ function ListSection() {
 			console.log(error);
 		}
 	};
-	getFolderData();
 
 	useEffect(() => {
 		getFolderData().then((data) => {
@@ -38,12 +75,17 @@ function ListSection() {
 
 	return (
 		<Container>
-			<SearchBar />
-			{links?.map((link) => (
-				<CardContainer key={link.id}>
-					<Card link={link} />
-				</CardContainer>
-			))}
+			<SearchWrapper>
+				<SearchBar placeholder='링크를 검색해 보세요' />
+				<SearchIcon>
+					<img src='/images/search-icon.png' alt='search-icon' />
+				</SearchIcon>
+			</SearchWrapper>
+			<CardContainer>
+				{links?.map((link) => (
+					<Card key={link.id} link={link} />
+				))}
+			</CardContainer>
 		</Container>
 	);
 }
