102f37d7be463089f9c86e94a029fac6a23be1df
youngeun <00eun1128@gmail.com>
2023-11-27 23:39:45 +0900

    chore 필요없는 파일 삭제

--- src/commons/api.js
+++
@@ -1,5 +0,0 @@
-export default async function getReviews() {
-  const response = await fetch("https://bootcamp-api.codeit.kr/docs/api/sample/user");
-  const body = await response.json();
-  return body;
-}
