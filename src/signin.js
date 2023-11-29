049358d556d7d869000acbd50660d61cc337a576
kimminhee <kimminhee@kimminheeui-MacBookAir.local>
2023-11-30 00:15:03 +0900

    feat: 코드 정리 및 footer & navigationbar 코드 작성

--- src/signin.js
+++ src/signin.js
@@ -3,17 +3,23 @@ import {
   removeInputError,
   isEmailValid,
   togglePassword,
-  redirectToIfAccessTokenExists,
+  redirectToIfAccessTokenExists
 } from "./utils.js";
-
+import "./landing.css";
+import "./global.css";
 redirectToIfAccessTokenExists("/folder");
 
 const emailInput = document.querySelector("#email");
 const emailErrorMessage = document.querySelector("#email-error-message");
-emailInput.addEventListener("focusout", (event) => validateEmailInput(event.target.value));
+emailInput.addEventListener("focusout", (event) =>
+  validateEmailInput(event.target.value)
+);
 function validateEmailInput(email) {
   if (email === "") {
-    setInputError({ input: emailInput, errorMessage: emailErrorMessage }, "이메일을 입력해주세요.");
+    setInputError(
+      { input: emailInput, errorMessage: emailErrorMessage },
+      "이메일을 입력해주세요."
+    );
     return;
   }
   if (!isEmailValid(email)) {
@@ -28,7 +34,9 @@ function validateEmailInput(email) {
 
 const passwordInput = document.querySelector("#password");
 const passwordErrorMessage = document.querySelector("#password-error-message");
-passwordInput.addEventListener("focusout", (event) => validatePasswordInput(event.target.value));
+passwordInput.addEventListener("focusout", (event) =>
+  validatePasswordInput(event.target.value)
+);
 function validatePasswordInput(password) {
   if (password === "") {
     setInputError(
@@ -37,7 +45,10 @@ function validatePasswordInput(password) {
     );
     return;
   }
-  removeInputError({ input: passwordInput, errorMessage: passwordErrorMessage });
+  removeInputError({
+    input: passwordInput,
+    errorMessage: passwordErrorMessage
+  });
 }
 
 const passwordToggleButton = document.querySelector("#password-toggle");
@@ -54,12 +65,12 @@ async function submitForm(event) {
     const response = await fetch("https://bootcamp-api.codeit.kr/api/sign-in", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
         email: emailInput.value,
-        password: passwordInput.value,
-      }),
+        password: passwordInput.value
+      })
     });
 
     if (!response.ok) {
@@ -75,7 +86,10 @@ async function submitForm(event) {
     localStorage.setItem("accessToken", accessToken);
     location.href = "/folder";
   } catch {
-    setInputError({ input: emailInput, errorMessage: emailErrorMessage }, "이메일을 확인해주세요.");
+    setInputError(
+      { input: emailInput, errorMessage: emailErrorMessage },
+      "이메일을 확인해주세요."
+    );
     setInputError(
       { input: passwordInput, errorMessage: passwordErrorMessage },
       "비밀번호를 확인해주세요."
