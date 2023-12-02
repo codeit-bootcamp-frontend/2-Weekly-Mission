6885a0b73860d2aa2fba5c0839f762394b627b6c
박지선 <shanshan@MacBook-Pro-6.local>
2023-12-02 14:06:26 +0900

    feat: html 골자 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import logoImg from "./images/logo.svg";
+import codeItProfile from "./images/codeit-profile.svg";
+import facebookImg from "./images/facebook.svg";
+import twitterImg from "./images/twitter.svg";
+import youtubeImg from "./images/youtube.svg";
+import instagramImg from "./images/instagram.svg";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <nav>
+          <img src={logoImg} alt="logo" />
+          <span>Codeit@codeit.com</span>
+        </nav>
+        <img src={codeItProfile} alt="codeit profile" />
+        <div>@코드잇</div>
+        <div>⭐️ 즐겨찾기</div>
+      </header>
+      <main>
+        <div>(search bar 들어갈 자리)</div>
+        <div>(링크 카드들 들어갈 자리)</div>
+      </main>
+
+      <footer>
+        <span>@codeit - 2023</span>
+        <div>
+          <a href="#">Privacy Policy</a>
+          <a href="#">FAQ</a>
+        </div>
+        <div>
+          <img src={facebookImg} alt="facebook" />
+          <img src={twitterImg} alt="twitter" />
+          <img src={youtubeImg} alt="youtube" />
+          <img src={instagramImg} alt="instagram" />
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default App;
