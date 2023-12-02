f4bc47eec9cb76295fd5fc8a16715d115c077dc4
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-12-02 15:02:12 +0900

    <feat>: 저장된 링크가 없을 때 보여주는 NoLink 컴포넌트 생성

---
+++ src/components/NoLink.js
@@ -0,0 +1,11 @@
+import '../styles/NoLink.css';
+
+const NoLink = () => {
+  return(
+    <section className="no-link--section">
+      <div className="no-link--description">저장된 링크가 없습니다</div>
+    </section>
+  )
+};
+
+export default NoLink;
--- src/components/Page/FolderPage.js
+++ src/components/Page/FolderPage.js
@@ -1,12 +1,14 @@
 import AddLinkForm from "../AddLinkForm";
 import '../../styles/Folder.css';
 import SearchBar from "../SearchBar";
+import NoLink from "../NoLink";
 
 const FolderPage = () => {
   return(
     <main className="contents folder-link--main">
       <AddLinkForm />
       <SearchBar/>
+      <NoLink />
       {/* <FolderOwner owner={owner} name={name} />
       <section className="card--section">
         {links.map((link) => <Card key={link.id} link={link} />)}
