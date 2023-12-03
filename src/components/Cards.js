bf2a3d1957eef78c310431cde33fed6a518d5f2a
김윤미 <younmeet@gmail.com>
2023-12-03 23:02:28 +0900

    feat : sinceCreatedDate 함수 구현

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,38 @@
 import cardDefualtImg from '../images/card_default.png';
 import '../styles/Cards.css'
 
+const MINUTE = 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
+const MONTH = DAY * 31;
+const YEAR = MONTH * 12;
+
 function formateDate(value) {
   const date = new Date(value)
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`
 }
+
+function sinceCreatedDate(value) {
+  const createdDate = new Date(value);
+  const date = new Date();
+
+  let timeDiff = date - createdDate;
+  timeDiff = timeDiff / 1000;
+
+  if (timeDiff < 2 * MINUTE) return "1 minute ago";
+  if (timeDiff < HOUR)
+    return `${Math.floor(timeDiff / MINUTE)} minutes ago`;
+  if (timeDiff < 2 * HOUR) return "1 hour ago";
+  if (timeDiff < DAY) return `${Math.floor(timeDiff / HOUR)} hours ago`;
+  if (timeDiff < 2 * DAY) return "1 day ago";
+  if (timeDiff < MONTH) return `${Math.floor(timeDiff / DAY)} days ago`;
+  if (timeDiff < 2 * MONTH) return "1 month ago";
+  if (timeDiff < YEAR)
+    return `${Math.floor(timeDiff / MONTH)} months ago`;
+  if (timeDiff < 2 * YEAR) return "1 year ago";
+  if (timeDiff >= 2 * YEAR) return `${timeDiff / YEAR} years ago`;
+}
+
 
 function Cards({ folderProfile }) { 
   const { links } = folderProfile;
@@ -19,7 +47,7 @@ function Cards({ folderProfile }) {
                 {<img src={item.imageSource || cardDefualtImg} alt="썸네일 이미지" />}
               </div>
               <div className="card-text">
-                <span className="created">10 minutes ago</span>
+                <span className="created">{sinceCreatedDate(item.createdAt)}</span>
                 <button className="button-kebab" type="button"></button>
                 <p className="description">{item.description}</p>
                 <span className="date">{formateDate(item.createdAt)}</span>
