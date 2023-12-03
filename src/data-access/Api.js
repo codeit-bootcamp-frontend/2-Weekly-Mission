ff8c5fc68167126507f99deac4b98fd3aa01795b
남숙희 <131663155+sooki88@users.noreply.github.com>
2023-12-03 18:03:28 +0900

    Update Api.js
    폴더 전체 링크 받아오기

--- src/data-access/Api.js
+++ src/data-access/Api.js
@@ -1,5 +1,6 @@
 const BASE_URL = "https://bootcamp-api.codeit.kr/api";
 
+// 유저 데이터 받아오기
 export async function getUserData() {
   const response = await fetch(`${BASE_URL}/users/1`);
   if (!response.ok) {
@@ -10,4 +11,16 @@ export async function getUserData() {
   const { data } = result;
   const userData = data[0];
   return userData;
+}
+
+// 폴더 '전체' 링크 받아오기
+export async function getFolderLink() {
+  const response = await fetch(`${BASE_URL}/users/1/links`);
+  if (!response.ok) {
+    throw new Error("폴더 링크 데이터를 불러오는데 실패했습니다.");
+  }
+
+  const result = await response.json();
+  const { data } = result;
+  return data;
 }
