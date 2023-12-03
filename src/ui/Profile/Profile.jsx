d3be5cc6e0f028cb7726e7b538a16876d7b5011e
남숙희 <131663155+sooki88@users.noreply.github.com>
2023-12-03 11:48:55 +0900

    Profile.jsx수정

--- src/ui/Profile/Profile.jsx
+++ src/ui/Profile/Profile.jsx
@@ -1,14 +1,14 @@
 import "./Profile.css";
 
-export const Profile = ({ profile }) => {
+export const Profile = (profile) => {
+  const email = profile.profile.profile.email;
+  const imageSource = profile.profile.profile.image_source;
+  // 왜 이렇게 길어진 걸까요?
+
   return (
     <div className="Profile">
-      <img
-        className="Profile-image"
-        src={profile.profileImageSource}
-        alt="프로필 이미지"
-      />
-      <span className="Profile-email">{profile.email}</span>
+      <img className="Profile-image" src={imageSource} alt="프로필 이미지" />
+      <span className="Profile-email">{email}</span>
     </div>
   );
 };
