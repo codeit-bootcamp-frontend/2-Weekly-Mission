90464894beef61bafd9ab0b5f95d16f59599f203
남숙희 <131663155+sooki88@users.noreply.github.com>
2023-12-04 16:47:44 +0900

    전체목록일땐 options 숨기기

--- ui/CardTabTitleOtions/CardTabTitleOtions.jsx
+++ ui/CardTabTitleOtions/CardTabTitleOtions.jsx
@@ -72,62 +72,8 @@ export function CardTabTitleOptions({ links }) {
       </div>
       <div className="titleOptions">
         <h2 className="title">{tabName}</h2>
-        <Options />
+        {clickTabAll ? [] : <Options />}
       </div>
     </>
   );
 }
-
-// 보관
-
-// import { Options } from "../Options/Options";
-// import "./CardTabTitleOptions.css";
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-
-// const BASE_URL = "https://bootcamp-api.codeit.kr/api";
-
-// export function CardTabTitleOptions() {
-//   const [tabList, setTabList] = useState([]);
-//   const [clickTabId, setClickTabId] = useState(0);
-
-//   // tab목록 가져오기
-//   async function fetchTabList() {
-//     try {
-//       const response = await axios.get(`${BASE_URL}/users/1/folders`);
-//       const { data } = response;
-//       setTabList(data.data);
-//     } catch (error) {
-//       throw new Error(error);
-//     }
-//   }
-
-//   useEffect(() => {
-//     fetchTabList();
-//   }, []);
-
-//   const handleSelectTab = (e) => {
-//     setClickTabId(e.target.id);
-//   };
-
-//   return (
-//     <>
-//       <div className="CardTabArr">
-//         {tabList.map((tab) => (
-//           <li
-//             key={tab.id}
-//             id={tab.id}
-//             className={tab.id === Number(clickTabId) ? "tab focused" : "tab"}
-//             onClick={handleSelectTab}
-//           >
-//             {tab.name}
-//           </li>
-//         ))}
-//       </div>
-//       <div className="titleOptions">
-//         <h2 className="title">유</h2>
-//         <Options />
-//       </div>
-//     </>
-//   );
-// }
