5a243b154f26f9f806889faa738238d08d2da328
KimByeongHwa <iambyeonghwa@gmail.com>
2023-12-05 05:09:06 +0900

    feat: AddLinkSection 제작(extending styled 관련 문제)

--- src/components/layouts/folder/AddLinkSection.tsx
+++ src/components/layouts/folder/AddLinkSection.tsx
@@ -1,5 +1,51 @@
+import styled from 'styled-components';
+import theme from '../../../styles/theme';
+import SearchBar from '../../molecules/SearchBar';
+import GradationButton from '../../molecules/GradationButton';
+
+const Container = styled.div`
+	width: 100%;
+	height: 21.9rem;
+	background-color: ${({ theme }) => theme.colors.skyblue};
+`;
+
+const SearchBarWrapper = styled.div`
+	width: 60%;
+	margin: 0 auto;
+	padding-top: 6rem;
+	display: flex;
+	align-items: center;
+	position: relative;
+`;
+
+// DOESN'T WORK
+// const StyledSearchBar = styled(SearchBar)`
+// 	padding-top: 6rem;
+// `;
+
+// DOESN'T WORK
+const StyledGradationButton = styled(GradationButton)`
+	position: absolute;
+	right: 0;
+`;
+
 function AddLinkSection() {
-	return <div>AddLinkSection</div>;
+	return (
+		<Container>
+			<SearchBarWrapper>
+				<SearchBar
+					inputWidth='75%'
+					backgroundColor='#fff'
+					borderColor='#6D6AFE'
+					placeholder='링크를 추가해 보세요.'
+					placeholderColor={theme.colors.gray60}
+					iconSrc='/images/link.png'
+					iconAlt='link-icon'
+				/>
+				<StyledGradationButton width='8rem'>추가하기</StyledGradationButton>
+			</SearchBarWrapper>
+		</Container>
+	);
 }
 
 export default AddLinkSection;
--- src/components/molecules/SearchBar.tsx
+++ src/components/molecules/SearchBar.tsx
@@ -1,74 +1,80 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { SearchBarProps } from '../../utils/interfaces';
 
 const Container = styled.div<SearchBarProps>`
-  width: ${({ width }) => width || '100%'};
-  height: ${({ height }) => height || '5.4rem'};
-  border: ${({ borderColor }) => `1px solid ${borderColor}` || 'none'};
-  border-radius: 10px;
-  background-color: ${({ backgroundColor }) => backgroundColor || '#f5f5f5'};
-  position: relative;
-  display: flex;
-  align-items: center;
-  padding: 1.5rem 1.6rem;
+	width: ${({ width }) => width || '100%'};
+	height: ${({ height }) => height || '5.4rem'};
+	border: ${({ borderColor }) => `1px solid ${borderColor}` || 'none'};
+	border-radius: 10px;
+	background-color: ${({ backgroundColor }) => backgroundColor || '#f5f5f5'};
+	position: relative;
+	display: flex;
+	align-items: center;
+	padding: 1.5rem 1.6rem;
 `;
 
 const Bar = styled.input<SearchBarProps>`
-  width: ${({ inputWidth }) => inputWidth || '100%'};
-  background-color: ${({ backgroundColor }) => backgroundColor || '#f5f5f5'};
-  border: none;
-  outline: none;
-  font-size: ${({ fontSize }) => fontSize || '1.6rem'};
-  color: ${({ fontColor }) => fontColor};
-  margin-left: 2.6rem;
+	width: ${({ inputWidth }) => inputWidth || '100%'};
+	background-color: ${({ backgroundColor }) => backgroundColor || '#f5f5f5'};
+	border: none;
+	outline: none;
+	font-size: ${({ fontSize }) => fontSize || '1.6rem'};
+	color: ${({ fontColor }) => fontColor};
+	margin-left: 2.6rem;
 
-  &::placeholder {
-    color: ${({ placeholderColor }) => placeholderColor};
-  }
+	&::placeholder {
+		color: ${({ placeholderColor }) => placeholderColor};
+	}
 `;
 
 const SearchIcon = styled.div<SearchBarProps>`
-  width: ${({ iconWidth }) => iconWidth || '1.6rem'};
-  height: ${({ iconHeight }) => iconHeight || '1.6rem'};
-  position: absolute;
-  cursor: pointer;
+	width: ${({ iconWidth }) => iconWidth || '1.6rem'};
+	height: ${({ iconHeight }) => iconHeight || '1.6rem'};
+	position: absolute;
+	cursor: pointer;
 
-  img {
-    width: 100%;
-    height: 100%;
-    text-align: center;
-  }
+	img {
+		width: 100%;
+		height: 100%;
+		text-align: center;
+	}
 `;
 
 function SearchBar({
-  width,
-  height,
-  backgroundColor,
-  borderColor,
-  inputWidth,
-  fontSize,
-  fontColor,
-  placeholder,
-  placeholderColor,
-  iconSrc,
-  iconAlt,
-  iconWidth,
-  iconHeight,
+	width,
+	height,
+	backgroundColor,
+	borderColor,
+	inputWidth,
+	fontSize,
+	fontColor,
+	placeholder,
+	placeholderColor,
+	iconSrc,
+	iconAlt,
+	iconWidth,
+	iconHeight,
 }: SearchBarProps) {
-  return (
-    <Container width={width} height={height} backgroundColor={backgroundColor} borderColor={borderColor}>
-      <Bar
-        inputWidth={inputWidth}
-        fontSize={fontSize}
-        fontColor={fontColor}
-        placeholder={placeholder || '링크를 검색해 보세요'}
-        placeholderColor={placeholderColor}
-      />
-      <SearchIcon iconWidth={iconWidth} iconHeight={iconHeight}>
-        <img src={iconSrc || '/images/search-icon.png'} alt={iconAlt || 'search-icon'} />
-      </SearchIcon>
-    </Container>
-  );
+	return (
+		<Container
+			width={width}
+			height={height}
+			backgroundColor={backgroundColor}
+			borderColor={borderColor}
+		>
+			<Bar
+				inputWidth={inputWidth}
+				backgroundColor={backgroundColor}
+				fontSize={fontSize}
+				fontColor={fontColor}
+				placeholder={placeholder || '링크를 검색해 보세요'}
+				placeholderColor={placeholderColor}
+			/>
+			<SearchIcon iconWidth={iconWidth} iconHeight={iconHeight}>
+				<img src={iconSrc || '/images/search-icon.png'} alt={iconAlt || 'search-icon'} />
+			</SearchIcon>
+		</Container>
+	);
 }
 
 export default SearchBar;
