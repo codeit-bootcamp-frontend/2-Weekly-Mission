11029fa26097e9ef4b44e32ce11e2a1ad250e728
남숙희 <131663155+sooki88@users.noreply.github.com>
2023-12-04 16:53:08 +0900

    App에 라우터 사용하기

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Folder from "./pages/Folder";
 import "./color.css";
 import "./reset.css";
 
 function App() {
-  return <Folder />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="folder" element={<Folder />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
