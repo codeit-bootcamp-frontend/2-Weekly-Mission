511fc4a75902c93d50a848c9be21d06169522ffe
JunCastle <juncastle97@gmail.com>
2023-12-07 14:23:03 +0900

    Fix: AddLink Button을 input 오른쪽에 고정

--- src/components/AddLink.js
+++ src/components/AddLink.js
@@ -3,8 +3,10 @@
 function AddLink() {
   return (
     <div className="AddLink">
-    <input className="AddLinkInput" placeholder="링크를 추가해 보세요"/>
-    <button className="AddLinkButton">추가하기</button>
+    <div className="InputContainer">
+      <input className="AddLinkInput" placeholder="링크를 추가해 보세요"/>
+      <button className="AddLinkButton">추가하기</button>
+    </div>
     </div>
   );
 }
