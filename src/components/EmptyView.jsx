a1a2cb43e1896f56738a9d92c87afcc0f4212452
김윤미 <younmeet@gmail.com>
2023-12-28 23:26:17 +0900

    feat: EmptyView 컴포넌트 스타일 구현

---
+++ src/components/EmptyView.jsx
@@ -0,0 +1,11 @@
+import '../styles/EmptyView.css';
+
+function EmptyView() {
+  return (
+    <div className="empty-view">
+      <p>저장된 링크가 없습니다.</p>
+    </div>
+  );
+}
+
+export default EmptyView;
