f91d981d12d80380598c5c8861abc9ca48c1b328
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2023-12-28 16:23:51 +0900

    Build: CRA에서 react-ts 템플릿 vite로 변경 및 tailwindcss 설치

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className='text-9xl'>HELLO - 적용되나 봅시다</h1>
+  )
+}
+
+export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  mode: 'jit',
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'class',
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-      screens: {
-          sm: '480px',
-          md: '768px',
-          lg: '1025px',
-          xl: '1280px',
-          xl2: '1360px',
-      },
-      extend: {},
+    extend: {},
   },
   plugins: [],
 }
