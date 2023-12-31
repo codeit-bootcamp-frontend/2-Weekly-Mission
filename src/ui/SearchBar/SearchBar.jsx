5402cebfe75c5580c131b3a1ef28137f3e31a654
Lee Seo Young <seoyoung720@gmail.com>
2024-01-01 03:32:50 +0900

    feat : 가위표 버튼을 클릭하면 SearchBar의 value가 ""가 되도록 함

--- src/ui/SearchBar/SearchBar.jsx
+++ src/ui/SearchBar/SearchBar.jsx
@@ -12,6 +12,11 @@ export const SearchBar = ({ value, onChange, onEnterPressed }) => {
     }
   };
 
+  const handleCancelClick = () => {
+    onChange("");
+    onEnterPressed();
+  };
+
   return (
     <div className="SearchBar">
       <input
@@ -27,12 +32,13 @@ export const SearchBar = ({ value, onChange, onEnterPressed }) => {
         alt="검색창인 것을 알려주는 돋보기 아이콘"
         className="SearchBar-SearchIcon"
       />
-      <img
-        src={DELETE_IMAGE}
-        alt="검색창에 입력된 값을 삭제하는 가위표 아이콘"
-        className="SearchBar-DeleteIcon"
-        style={{ fill: "#666666" }}
-      />
+      <button onClick={handleCancelClick}>
+        <img
+          src={DELETE_IMAGE}
+          alt="검색창에 입력된 값을 삭제하는 가위표 아이콘"
+          className="SearchBar-DeleteIcon"
+        />
+      </button>
     </div>
   );
 };
