aa1bed6944170503d97ca0e80cbccc2e773b3486
Lee Seo Young <seoyoung720@gmail.com>
2024-01-01 02:29:16 +0900

    feat : SearchBar의 입력된 사항을 Enter키로 탐색해서 console에 올리는 handleSearchFolder를 FolderPage에 추가

--- src/page-layout/FolderPage/FolderPage.jsx
+++ src/page-layout/FolderPage/FolderPage.jsx
@@ -5,17 +5,44 @@ import { useGetFolder } from "data-access/useGetFolder";
 import { CardList } from "../../ui/CardList";
 import { ReadOnlyCard } from "../../ui/ReadOnlyCard";
 import { SearchBar } from "../../ui/SearchBar";
+import { useState } from "react";
 
 export const FolderPage = () => {
   const { data } = useGetFolder();
   const { links } = data || {};
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchFolder = () => {
+    console.log("++++++++++");
+    const searchValueLower = searchValue.toLowerCase().split(" ");
+    links.forEach((link) => {
+      if (
+        searchValueLower.every((value) =>
+          link.url.toLowerCase().includes(value)
+        ) ||
+        (link.title &&
+          searchValueLower.every((value) =>
+            link.title.toLowerCase().includes(value)
+          )) ||
+        searchValueLower.every((value) =>
+          link.description.toLowerCase().includes(value)
+        )
+      ) {
+        console.log(`${searchValue} 발견: ${link.description}`);
+      }
+    });
+  };
 
   return (
     <Layout>
       <div className="FolderPage">
         <div className="FolderPage-items">
           <Link to="/shared">sharedPage 가는 링크</Link>
-          <SearchBar />
+          <SearchBar
+            value={searchValue}
+            onChange={setSearchValue}
+            onEnterPressed={handleSearchFolder}
+          />
           <CardList>
             {links?.map((link) => (
               <ReadOnlyCard key={link?.id} {...link} />
--- src/ui/SearchBar/SearchBar.jsx
+++ src/ui/SearchBar/SearchBar.jsx
@@ -1,10 +1,27 @@
 import "./SearchBar.css";
 import { SEARCH_IMAGE } from "./constant";
 
-export const SearchBar = () => {
+export const SearchBar = ({ value, onChange, onEnterPressed }) => {
+  const handleInputChange = (e) => {
+    onChange(e.target.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      onEnterPressed();
+    }
+  };
+
   return (
     <div className="SearchBar">
-      <input className="SearchBar-input" type="search" placeholder="링크를 검색해 보세요." />
+      <input
+        className="SearchBar-input"
+        type="search"
+        placeholder="링크를 검색해 보세요."
+        value={value}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
+      />
       <img
         src={SEARCH_IMAGE}
         alt="검색창인 것을 알려주는 돋보기 아이콘"
