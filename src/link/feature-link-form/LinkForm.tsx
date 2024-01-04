18e17ec16b444fcde3a3d5d7b3dbd8db47dd8224
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-01-04 18:22:13 +0900

    feat: next 마이그레이션 - useGetFolders 커스텀 훅 구현

--- src/link/feature-link-form/LinkForm.tsx
+++ src/link/feature-link-form/LinkForm.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames/bind';
 import styles from './LinkForm.module.scss';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 
@@ -10,5 +11,10 @@ type LinkFormProps = {
 
 // 링크 입력하는 폼 컴포넌트
 export const LinkForm = ({ hideFixedLinkForm = false }: LinkFormProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false); // 모달 열림 여부
+  // getFolders
+  const [selectedFolderId, setSelectedFolderId] = useState<number | null>(null); // 선택된 폴더 아이디
+  const [linkUrl, setLinkUrl] = useState<string>(''); // 링크 주소
+
   return <div className={cx('container')}></div>;
 };
