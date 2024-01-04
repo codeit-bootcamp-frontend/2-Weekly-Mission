5a4947bba22de97b58d89f633654ecd6beff5eba
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-01-04 15:14:41 +0900

    feat: next 마이그레이션 - ReadOnlyCard 컴포넌트 구현

---
+++ src/link/ui-read-only-card/ReadOnlyCard.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+// 읽기 전용 카드 프롭 타입 선언
+type ReadOnlyCardProps = {
+  url: string;
+  imageSource: string;
+  alt: string;
+  elapsedTime: string;
+  description: string;
+  createdAt: string;
+};
+
+// readOnly를 따로 둔 이유는 모르겠으나 나중에 수정이 있을 것 같음(?)
+export const ReadOnlyCard = ({
+  url,
+  imageSource,
+  alt,
+  elapsedTime,
+  description,
+  createdAt,
+}: ReadOnlyCardProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const handleMouseOver = () => setIsHovered(true);
+  const handleMouseLeave = () => setIsHovered(false);
+
+  return (
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      {
+        // 카드
+        // 카드 이미지
+        // 카드 컨텐츠
+      }
+    </a>
+  );
+};
