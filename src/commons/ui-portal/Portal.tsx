544da82e7f697113ee0c0ddb3552e1a339e098a4
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-01-04 17:47:51 +0900

    feat: next 마이그레이션 - Portal 컴포넌트 구현

---
+++ src/commons/ui-portal/Portal.tsx
@@ -0,0 +1,22 @@
+import { ReactNode, useLayoutEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+type PortalProps = {
+  children: ReactNode;
+  container?: Element | DocumentFragment | null;
+};
+
+// useLayoutEffect : dom을 조작할 때 깜빡임을 없애기 위해 사용한 것으로 보임
+// createPortal : 일부 자식들을 DOM의 다른 부분으로 렌더링. 즉 모달용으로 사용한 것으로 보임.
+
+export const Portal = ({ children, container }: PortalProps) => {
+  const [mountNode, setMountNode] = useState<Element | DocumentFragment | null>(
+    null
+  );
+
+  useLayoutEffect(() => {
+    setMountNode(container || document.body);
+  }, [container]);
+
+  return mountNode ? createPortal(children, mountNode) : mountNode;
+};
