743ca07fb8cc8b61200084caad72f72fbe2d33c9
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-01-04 12:44:21 +0900

    add: next 마이그레이션 - 이미지 에셋 이전

--- components/sharing/feature-layout/Layout.tsx
+++ components/sharing/feature-layout/Layout.tsx
@@ -21,7 +21,9 @@ export const Layout = ({
 }: LayoutProps) => {
   return (
     <div>
+      {/* 네비게이션 바 컴포넌트 */}
       <main className={cx('main')}>{children}</main>
+      {/* 푸터 컴포넌트 */}
     </div>
   );
 };
