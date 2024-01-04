a100c9677ef3f395db0379cdda19f35b549e8e4a
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-01-04 13:05:44 +0900

    feat: next 마이그레이션 - 네비게이션 바 구현

--- components/commons/ui-navigation-bar/NavigationBar.tsx
+++ components/commons/ui-navigation-bar/NavigationBar.tsx
@@ -1,5 +1,10 @@
 import styles from './NavigationBar.module.scss';
 import classNames from 'classnames/bind';
+import Link from 'next/link';
+import { ROUTE } from '../util/constant';
+import { Cta } from '../ui-cta/Cta';
+import { Profile } from '../ui-profile/Profile';
+import { LOGO_IMAGE, TEXT } from './constant';
 
 const cx = classNames.bind(styles);
 
@@ -14,7 +19,20 @@ type NavigationBarProps = {
 export const NavigationBar = ({ profile, isSticky }: NavigationBarProps) => {
   return (
     <nav className={cx('container', { sticky: isSticky })}>
-      <div className={cx('items')}></div>
+      <div className={cx('items')}>
+        <Link href={ROUTE.랜딩}>
+          <img className={cx('logo')} src={LOGO_IMAGE} alt="Linkbrary 로고" />
+        </Link>
+        {profile ? (
+          <Profile profile={profile} />
+        ) : (
+          <Link href={ROUTE.로그인}>
+            <Cta>
+              <span className={cx('signin')}>{TEXT.login}</span>
+            </Cta>
+          </Link>
+        )}
+      </div>
     </nav>
   );
 };
