fa0dfb13596875043d91f3e1a4924cc8493b7ff7
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-01-06 13:38:57 +0900

    chore: PasswordInput 의사 코드 작성

--- src/commons/ui-password-input/PasswordInput.tsx
+++ src/commons/ui-password-input/PasswordInput.tsx
@@ -1,5 +1,14 @@
 import { Input } from '../ui-input/Input';
 
+/* PasswordInput의 프롭 */
+// 기존 Input - value, onChange, placeholder
+//
+// 에러가 뜰 때 - , 에러 여부, 에러 메세지
+
+// PasswordInput의 state
+// + type은 text나 password 중 하나 필수
+// 비밀번호 가림 여부 => 타입 바뀌어야됨(password, text)
+
 export default function PasswordInput() {
   return (
     <div>
