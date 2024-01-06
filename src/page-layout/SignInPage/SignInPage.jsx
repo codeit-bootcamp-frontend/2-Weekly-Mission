cfb7e852544132fe71780a53e875335b83203b2d
Lee Seo Young <seoyoung720@gmail.com>
2024-01-06 15:55:18 +0900

    style : 스타일 오류 수정

--- src/page-layout/SignInPage/SignInPage.jsx
+++ src/page-layout/SignInPage/SignInPage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const SignInPage = () => {
   return (
-    <div className="body">
+    <div className="signIn">
       <header>
         <Link className="logo-link" to="../">
           <img
