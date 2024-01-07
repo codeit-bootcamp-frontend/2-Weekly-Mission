8577940849f28640b3a8848f572639ce1a62fe8b
Jisoo Han <llqrw55@naver.com>
2024-01-08 02:43:42 +0900

    fix:CloseButton 수정

--- components/commons/modals/CloseButton.tsx
+++ components/commons/modals/CloseButton.tsx
@@ -2,12 +2,6 @@ import styled from "styled-components";
 import Image from "next/image";
 
 const Button = styled.div`
-  width: 2.4rem;
-  height: 2.4rem;
-  border-radius: 50%;
-  background-color: #e7effb;
-  color: #9fa6b2;
-  padding: 0.7rem;
   cursor: pointer;
   position: absolute;
   top: 1.6rem;
