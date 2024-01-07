4ff8c49f62b530cc0bd11d1744eb49dcc1b1ede5
Jisoo Han <llqrw55@naver.com>
2024-01-08 02:51:44 +0900

    fix:CloseButton 재수정

--- components/commons/modals/CloseButton.tsx
+++ components/commons/modals/CloseButton.tsx
@@ -1,24 +1,23 @@
 import styled from "styled-components";
 import Image from "next/image";
 
-const Button = styled.div`
-  cursor: pointer;
-  position: absolute;
-  top: 1.6rem;
-  right: 1.6rem;
-`;
-
 function CloseButton({ closeModal }: { closeModal: () => void }) {
   return (
-    <Button onClick={closeModal}>
-      <Image
-        width={24}
-        height={24}
-        src="/images/modal-close.png"
-        alt="닫기 아이콘"
-      />
-    </Button>
+    <Button
+      width={24}
+      height={24}
+      src="/images/modal-close.png"
+      alt="닫기 아이콘"
+      onClick={closeModal}
+    />
   );
 }
+
+const Button = styled(Image)`
+  cursor: pointer;
+  position: absolute;
+  top: 1rem;
+  right: 1.6rem;
+`;
 
 export default CloseButton;
