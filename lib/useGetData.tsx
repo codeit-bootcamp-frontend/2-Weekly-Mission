2d215b2722f83250a80cf7da20ece3e9dbad03e3
Chyyni <catharina0409@gmail.com>
2024-01-07 21:47:59 +0900

    데이터를 가져오는 훅 생성

---
+++ lib/useGetData.tsx
@@ -0,0 +1,26 @@
+import axios from '@/lib/axios.js';
+import { useEffect, useState } from 'react';
+
+export default function useGetData(url: string, dependency: string = ''): [boolean, string, Array<object>] {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+  const [data, setData] = useState<object[]>([]);
+
+  const getData = async () => {
+    try {
+      const res = await axios.get(url);
+      const data = res.data.data;
+      setData(data);
+    } catch (error) {
+      setError('error');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [dependency]);
+
+  return [loading, error, data];
+}
