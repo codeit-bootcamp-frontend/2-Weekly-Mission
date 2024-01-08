af852362335d8eb7c0e34b2ba127d3933bea2e9d
designsoo <kheesoo10@gmail.com>
2024-01-09 01:41:49 +0900

    feat: Implement search form components

---
+++ src/components/common/SearchForm/index.jsx
@@ -0,0 +1,32 @@
+import { useRef } from 'react';
+import Image from 'next/image';
+
+import styles from './SearchForm.module.scss';
+import classNames from 'classnames/bind';
+
+import { ICON } from 'stores/importImg';
+
+const cx = classNames.bind(styles);
+const { search } = ICON;
+
+const SearchForm = ({ setKeyword }) => {
+  const inputRef = useRef();
+
+  const handleChange = () => setKeyword(inputRef.current.value);
+
+  return (
+    <form className={cx('search-form')}>
+      <Image src={search.url} alt={search.alt} className={cx('search-form-icon')} />
+      <input
+        ref={inputRef}
+        type='search'
+        name='q'
+        placeholder='링크를 검색해 보세요.'
+        aria-label='링크 검색 바'
+        onChange={handleChange}
+        className={cx('search-form-input')}
+      />
+    </form>
+  );
+};
+export default SearchForm;
