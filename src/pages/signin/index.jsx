02cd7411e9e6761817d4cdc45b18f085313439d6
designsoo <kheesoo10@gmail.com>
2024-01-09 01:51:14 +0900

    feat: Apply implemented components to pages

---
+++ src/pages/signin/index.jsx
@@ -0,0 +1,12 @@
+import Input from 'components/common/TextField/Input';
+
+const Signin = () => {
+  return (
+    <div>
+      <form action=''>
+        <Input />
+      </form>
+    </div>
+  );
+};
+export default Signin;
