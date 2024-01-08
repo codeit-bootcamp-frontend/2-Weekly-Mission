439eca0073398173a633340fa46bcf01cfea3d96
Chyyni <catharina0409@gmail.com>
2024-01-08 15:01:37 +0900

    모달 레이아웃 생성

---
+++ components/ModalLayout/ModalLayout.tsx
@@ -0,0 +1,51 @@
+import React, { MouseEvent, ReactNode, useEffect, useRef } from 'react';
+import Image from 'next/image';
+import CloseImg from '@/public/images/close.svg';
+import styles from './ModalLayout.module.css';
+
+interface Props {
+  setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  modalTitle: string;
+  details?: string;
+  children: ReactNode;
+}
+
+export default function ModalLayout({ setModalOpen, modalTitle, details, children }: Props) {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  // todo: 여기서 이벤트 타입은 ?
+  useEffect(() => {
+    const handleModalClose = (e) => {
+      if (modalRef.current && !modalRef.current.contains(e.target)) {
+        setModalOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleModalClose);
+
+    return () => {
+      document.removeEventListener('mousedown', handleModalClose);
+    };
+  }, []);
+
+  return (
+    <div className={styles.background}>
+      <div className={styles.container} ref={modalRef}>
+        <button
+          className={styles.closeButton}
+          onClick={(e) => {
+            e.preventDefault();
+            setModalOpen(false);
+          }}
+        >
+          <Image className={styles.closeImage} src={CloseImg} alt='모달 닫기' width={12} height={12} />
+        </button>
+        <header className={styles.header}>
+          <p className={styles.modalTitle}>{modalTitle}</p>
+          <p className={styles.details}>{details}</p>
+        </header>
+        {children}
+      </div>
+    </div>
+  );
+}
