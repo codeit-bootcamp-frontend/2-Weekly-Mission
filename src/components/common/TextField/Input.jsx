6648a413e79fc9bad2b97dffead2d68c43f48beb
designsoo <kheesoo10@gmail.com>
2024-01-10 00:08:58 +0900

    feat: Implement text-field component

---
+++ src/components/common/TextField/Input.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+
+import styles from './Input.module.scss';
+import classNames from 'classnames/bind';
+import IconButton from 'components/common/Button/IconButton';
+
+import { PASSWORD_SHOW_MODE } from 'stores/auth';
+
+const cx = classNames.bind(styles);
+
+const Input = ({ error = false, type = 'text', ...props }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const { iconEye, inputType, passwordShowMode } = isVisible
+    ? PASSWORD_SHOW_MODE.on
+    : PASSWORD_SHOW_MODE.off;
+
+  const handlePasswordToggleClick = () => {
+    setIsVisible((prev) => !prev);
+  };
+
+  return (
+    <div>
+      <div className={cx('input-group')}>
+        <input
+          name={type}
+          type={inputType}
+          {...props}
+          className={cx('input', { error: error })}
+        />
+
+        {type === 'password' && (
+          <IconButton
+            svg={iconEye}
+            alt={iconEye}
+            iconSize={16}
+            aria-label={passwordShowMode}
+            role='switch'
+            aria-checked={isVisible}
+            onClick={handlePasswordToggleClick}
+            className={cx('btn-password-toggle')}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+export default Input;
