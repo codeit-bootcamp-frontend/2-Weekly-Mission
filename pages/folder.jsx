74eac4419a2554179a51a32a48115c404026a1ef
sobokki <jso4093@naver.com>
2024-01-09 14:16:03 +0900

    refactor 11주차 리펙토링 중

---
+++ pages/folder.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header/Header";
+import Nav from "../components/Nav/Nav";
+export default function Folder() {
+  return (
+    <>
+      {" "}
+      <Nav />
+      <Header />
+    </>
+  );
+}
