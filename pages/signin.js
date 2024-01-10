049adfa000b044532192b468efdc530132148ba8
Lee Seo Young <seoyoung720@gmail.com>
2024-01-10 23:11:08 +0900

    style : signin.js에 스타일 적용

--- pages/signin.js
+++ pages/signin.js
@@ -16,26 +16,43 @@ export default function SignIn() {
         <Link className={styles.logoLink} href={"https://www.google.com/"}>
           image
         </Link>
-        <p>
+        <p className={styles.headerMessage}>
           회원이 아니신가요?
-          <Link href={"https://www.google.com/"}>회원 가입하기</Link>
+          <Link className={styles.headerLink} href={"https://www.google.com/"}>
+            회원 가입하기
+          </Link>
         </p>
       </header>
-      <div>
-        <form method="post" action="#" onSubmit={handleSubmit}>
-          <div>
+      <div className={styles.signBox}>
+        <form
+          className={styles.signForm}
+          method="post"
+          action="#"
+          onSubmit={handleSubmit}
+        >
+          <div className={styles.signInputs}>
             <h1>EmailInput</h1>
             <h1>PasswordInput</h1>
           </div>
-          <button type="submit" id="loginButton">
+          <button className={styles.cta} type="submit" id="loginButton">
             로그인
           </button>
         </form>
-        <div>
-          <span>소셜 로그인</span>
-          <div>
-            <Link href={"https://www.google.com/"}>to Google</Link>
-            <Link href={"https://www.google.com/"}>to Kakao</Link>
+        <div className={styles.snsBox}>
+          <span className={styles.snsText}>소셜 로그인</span>
+          <div className={styles.snsLinks}>
+            <Link
+              className={`${styles.snsLink} ${styles.googleLink}`}
+              href={"https://www.google.com/"}
+            >
+              to Google
+            </Link>
+            <Link
+              className={`${styles.snsLink} ${styles.kakaoLink}`}
+              href={"https://www.google.com/"}
+            >
+              to Kakao
+            </Link>
           </div>
         </div>
       </div>
