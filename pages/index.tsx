2487f06cf60ee613fed451e419fc5da89e2ca1a6
김윤미 <younmeet@gmail.com>
2024-01-10 21:30:28 +0900

    feat: Button 컴포넌트 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,15 @@
+import ButtonLink from '@/components/ButtonLink';
+import buttonLinkStyles from '@/components/ButtonLink.module.scss';
+
 export default function Home() {
   return (
     <>
+      <ButtonLink
+        href={'/signin'}
+        className={buttonLinkStyles['button-primary']}
+      >
+        로그인
+      </ButtonLink>
     </>
-  )
+  );
 }
