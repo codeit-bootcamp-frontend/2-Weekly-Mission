82ee996cce8f132702058639f68ac266be6ac820
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-01-10 12:47:51 +0900

    refactor(mentor): footer Link map이용 반복 최소화

--- src/commons/ui-footer/constant.js
+++ src/commons/ui-footer/constant.js
@@ -1,5 +1,7 @@
 export const TEXT = {
-  codeit: "©codeit - 2023",
-  privacyPolicy: "Privacy Policy",
-  faq: "FAQ",
+  codeit: '©codeit - 2023',
+  privacyPolicy: 'Privacy Policy',
+  faq: 'FAQ',
 };
+
+export const SNS = ['facebook', 'twitter', 'youtube', 'instagram'];
