54fde50bf3168392907bf3375da11f01849cbc5f
Lee Seo Young <seoyoung720@gmail.com>
2024-01-10 23:27:18 +0900

    feat : kakao 버튼 누르면 kakao 홈페이지로 연결

--- pages/signin.js
+++ pages/signin.js
@@ -1,4 +1,5 @@
 import styles from "@/styles/SignIn.module.css";
+import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 import { EmailInput, PasswordInput } from "@/components/Input";
@@ -13,66 +14,84 @@ export default function SignIn() {
   };
 
   return (
-    <div className={styles.signIn}>
-      <header className={styles.header}>
-        <Link className={styles.logoLink} href={"/"}>
-          <Image
-            src="/images/logo.svg"
-            width={133}
-            height={24}
-            alt="Linkbrary"
-          />
-        </Link>
-        <p className={styles.headerMessage}>
-          회원이 아니신가요?
-          <Link className={styles.headerLink} href={"https://www.google.com/"}>
-            회원 가입하기
+    <>
+      <Head>
+        <title>Login</title>
+        <meta property="og:title" content="Linkbrary" key="title" />
+        <meta
+          name="viewport"
+          content="width=device-width,minimum-scale=1,initial-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+        />
+      </Head>
+      <div className={styles.signIn}>
+        <header className={styles.header}>
+          <Link className={styles.logoLink} href={"/"}>
+            <Image
+              src="/images/logo.svg"
+              width={133}
+              height={24}
+              alt="Linkbrary"
+            />
           </Link>
-        </p>
-      </header>
-      <div className={styles.signBox}>
-        <form
-          className={styles.signForm}
-          method="post"
-          action="#"
-          onSubmit={handleSubmit}
-        >
-          <div className={styles.signInputs}>
-            <EmailInput />
-            <PasswordInput />
-          </div>
-          <button className={styles.cta} type="submit" id="loginButton">
-            로그인
-          </button>
-        </form>
-        <div className={styles.snsBox}>
-          <span className={styles.snsText}>소셜 로그인</span>
-          <div className={styles.snsLinks}>
+          <p className={styles.headerMessage}>
+            회원이 아니신가요?
             <Link
-              className={`${styles.snsLink} ${styles.googleLink}`}
+              className={styles.headerLink}
               href={"https://www.google.com/"}
             >
-              <Image
-                src="/images/google.png"
-                width={24}
-                height={24}
-                alt="Google Login"
-              />
+              회원 가입하기
             </Link>
-            <Link
-              className={`${styles.snsLink} ${styles.kakaoLink}`}
-              href={"https://www.google.com/"}
-            >
-              <Image
-                src="/images/kakao.svg"
-                width={24}
-                height={24}
-                alt="Kakao Login"
-              />
-            </Link>
+          </p>
+        </header>
+        <div className={styles.signBox}>
+          <form
+            className={styles.signForm}
+            method="post"
+            action="#"
+            onSubmit={handleSubmit}
+          >
+            <div className={styles.signInputs}>
+              <EmailInput />
+              <PasswordInput />
+            </div>
+            <button className={styles.cta} type="submit" id="loginButton">
+              로그인
+            </button>
+          </form>
+          <div className={styles.snsBox}>
+            <span className={styles.snsText}>소셜 로그인</span>
+            <div className={styles.snsLinks}>
+              <Link
+                className={`${styles.snsLink} ${styles.googleLink}`}
+                href={"https://www.google.com/"}
+              >
+                <Image
+                  src="/images/google.png"
+                  width={24}
+                  height={24}
+                  alt="Google Login"
+                />
+              </Link>
+              <Link
+                className={`${styles.snsLink} ${styles.kakaoLink}`}
+                href={"https://www.kakaocorp.com/page/"}
+              >
+                <Image
+                  src="/images/kakao.svg"
+                  width={24}
+                  height={24}
+                  alt="Kakao Login"
+                />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
