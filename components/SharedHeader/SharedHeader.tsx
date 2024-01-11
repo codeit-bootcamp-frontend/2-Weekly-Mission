b7272cc65d2926069b0cd54fb54d53ec8297c877
Chyyni <catharina0409@gmail.com>
2024-01-11 16:59:01 +0900

    feat: shared 페이지

---
+++ components/SharedHeader/SharedHeader.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import React from 'react';
+import styles from './SharedHeader.module.css';
+import useGetData from '@/lib/useGetData';
+
+interface Props {
+  userId: string | undefined;
+  folderId: string | string[] | undefined;
+}
+
+export default function SharedHeader({ userId, folderId }: Props) {
+  const [userLoading, userError, userData] = useGetData(`/user/${userId}`, userId);
+  const [folderLoading, folderError, folderData] = useGetData(`/folders/${folderId}`, folderId);
+
+  if (userLoading) return;
+  if (userError) return;
+  if (folderLoading) return;
+  if (folderError) return;
+
+  const { name: userName, image_source: imageSource } = userData[0];
+  const { name: folderName } = folderData[0];
+
+  return (
+    <div className={styles.profile}>
+      <Image className={styles.profileImage} src={imageSource} alt='사용자 프로필' width={60} height={60} />
+      <p className={styles.userName}>{userName}</p>
+      <p className={styles.folderName}>{folderName}</p>
+    </div>
+  );
+}
--- pages/shared.tsx
+++
@@ -1,5 +0,0 @@
-import Navbar from '@/components/Navbar/Navbar';
-
-export default function index() {
-  return <Navbar userId='1' className='sticky' />;
-}
