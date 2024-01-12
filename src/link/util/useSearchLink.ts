0b8464831d3e3a751e8ca6e2ee9c2b81bf5c8525
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-01-12 13:52:10 +0900

    refactor(mentor): 링크 검색 리바운싱 이용 최적화

--- src/link/util/useSearchLink.ts
+++ src/link/util/useSearchLink.ts
@@ -4,9 +4,20 @@ import { ChangeEventHandler, MouseEventHandler, useState } from 'react';
 // 링크 검색값을 반환하는 커스텀 훅
 export const useSearchLink = (links: Link[]) => {
   const [searchValue, setSearchValue] = useState('');
+  const [rebouncedSearchValue, setRebouncedSearchValue] = useState(''); // 리바운싱된 검색값
+  const [timer, setTimer] = useState<number | ReturnType<typeof setTimeout>>(0);
 
-  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+  const handleChange: ChangeEventHandler<HTMLInputElement> = async (e) => {
+    // 리바운싱
+    if (timer) {
+      clearTimeout(timer);
+    }
+    const newTimer = setTimeout(async () => {
+      setRebouncedSearchValue(searchValue);
+    }, 100);
+
     setSearchValue(e.target.value);
+    setTimer(newTimer);
   };
 
   const handleCloseClick: MouseEventHandler<HTMLButtonElement> = () => {
@@ -16,7 +27,7 @@ export const useSearchLink = (links: Link[]) => {
   //   배열 필터를 통해 결과를 저장
   const result = links.filter((link) => {
     // lowerCase로 검색
-    const searchValueLowerCase = searchValue?.toLowerCase();
+    const searchValueLowerCase = rebouncedSearchValue?.toLowerCase();
     const titleLowerCase = link?.title?.toLowerCase();
     const descriptionLowerCase = link?.description?.toLowerCase();
     const urlLowerCase = link?.url?.toLowerCase();
@@ -29,5 +40,10 @@ export const useSearchLink = (links: Link[]) => {
   });
 
   //   함수와 입력값, 결과 반환
-  return { searchValue, handleChange, handleCloseClick, result };
+  return {
+    searchValue,
+    handleChange,
+    handleCloseClick,
+    result,
+  };
 };
