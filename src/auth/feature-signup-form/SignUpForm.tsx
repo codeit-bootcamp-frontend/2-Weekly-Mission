1d742344805d412d606065b9176e129680be4123
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-01-12 11:53:13 +0900

    feat: 회원가입 페이지 완성

--- src/auth/feature-signup-form/SignUpForm.tsx
+++ src/auth/feature-signup-form/SignUpForm.tsx
@@ -5,16 +5,70 @@ import { Cta } from '@/src/commons/ui-cta/Cta';
 import PasswordInput from '@/src/commons/ui-password-input/PasswordInput';
 import { Controller, useForm } from 'react-hook-form';
 import { useCheckEmail } from '../utils/useCheckEmail';
+import { useSignUp } from '../utils/useSignUp';
+import { MouseEvent, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 const cx = classNames.bind(styles);
 
 export default function SignUpForm() {
-  const { watch, control } = useForm({
+  const router = useRouter();
+  const [isValid, setIsInvalid] = useState<boolean>(false);
+
+  const { watch, control, trigger, getFieldState } = useForm({
     defaultValues: { email: '', password: '', passwordRepeat: '' },
     mode: 'onBlur',
   });
 
   const checkEmail = useCheckEmail(watch('email'));
+  const signUp = useSignUp(watch('email'), watch('password'));
+
+  // 폼 제출 이벤트 콜백
+  const handleSubmit = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    // 각 인풋의 유효성 검사(useForm의 trigger사용)
+    trigger(['email', 'password', 'passwordRepeat']).then(() => {
+      if (
+        getFieldState('email').error ||
+        getFieldState('password').error ||
+        getFieldState('passwordRepeat').error
+      ) {
+        setIsInvalid(false);
+      } else {
+        setIsInvalid(true);
+      }
+    });
+  };
+
+  // 이미 로그인 되어있으면 리다이렉트
+  if (typeof window !== 'undefined') {
+    if (window.localStorage.getItem('accessToken')) {
+      router.push('/folder');
+    }
+  }
+
+  // 회원가입에 성공하면 로컬스토리지 저장 후 페이지 리다이렉트
+  if (signUp.data) {
+    const accessToken = signUp?.data?.data?.accessToken;
+    window.localStorage.setItem('accessToken', accessToken);
+
+    if (window.localStorage.getItem('accessToken')) {
+      router.push('/folder');
+    }
+  }
+
+  // 자체적으로 유효성 검증 후 에러가 존재한다면 요청하지 않음
+  useEffect(() => {
+    if (isValid) {
+      signUp.execute();
+    }
+
+    // 값이 올바른데도 오류가 나면 서버 문제로 가정
+    if (signUp.error) {
+      alert('요청하신 서버에 오류가 있습니다.');
+    }
+  }, [isValid]);
 
   return (
     <form className={cx('form')}>
@@ -76,6 +130,7 @@ export default function SignUpForm() {
           name="passwordRepeat"
           defaultValue=""
           rules={{
+            required: '비밀번호를 입력해 주세요',
             validate: (value) =>
               value === watch('password')
                 ? true
@@ -92,7 +147,9 @@ export default function SignUpForm() {
         />
       </div>
       <Cta className={cx('cta')}>
-        <button className={cx('button')}>회원가입</button>
+        <button className={cx('button')} onClick={handleSubmit}>
+          회원가입
+        </button>
       </Cta>
     </form>
   );
