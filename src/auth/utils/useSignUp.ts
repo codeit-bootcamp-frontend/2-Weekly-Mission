7e31acf6a88a7179aa70bb33a072a7ddb4ee5301
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2024-01-12 10:27:51 +0900

    feat: useSignUp 커스텀 훅 구현

--- src/auth/utils/useSignUp.ts
+++ src/auth/utils/useSignUp.ts
@@ -2,14 +2,14 @@ import { axiosInstance } from '@/src/commons/util/axiosInstance';
 import { useAsync } from '@/src/commons/util/useAsync';
 import { Token } from '../type';
 
-export const useSignIn = (email: string, password: string) => {
-  const signIn = () =>
-    axiosInstance.post<{ data: Token }>('sign-in', {
+export const useSignUp = (email: string, password: string) => {
+  const signUp = () =>
+    axiosInstance.post<{ data: Token }>('sign-up', {
       email: email,
       password: password,
     });
 
-  const { execute, loading, error, data } = useAsync(signIn, true);
+  const { execute, loading, error, data } = useAsync(signUp, true);
 
   return {
     execute,
