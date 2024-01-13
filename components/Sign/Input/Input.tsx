92a506e635b72e9896552cd5de36306865e7d8d6
Chyyni <catharina0409@gmail.com>
2024-01-13 18:23:41 +0900

    refactor: Input 컴포넌트 파일에 중복으로 선언되어 있던 불필요한 타입 삭제

--- components/Sign/Input/Input.tsx
+++ components/Sign/Input/Input.tsx
@@ -1,17 +1,12 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import { useFormContext } from 'react-hook-form';
+import { FormValues, validation } from '@/lib/types';
 import EyeOn from '@/public/images/eye-on.svg';
 import EyeOff from '@/public/images/eye-off.svg';
 import styles from './Input.module.css';
 
-export interface FormValues {
-  email: string;
-  password: string;
-  doubleCheckPassword: string;
-}
-
-interface InputProps {
+interface Props {
   label: '이메일' | '비밀번호' | '비밀번호 확인';
   id: 'email' | 'password' | 'doubleCheckPassword';
   type: 'email' | 'password';
@@ -22,18 +17,7 @@ interface InputProps {
   validation: validation;
 }
 
-export type validation = {
-  required: '이메일을 입력해주세요.' | '비밀번호를 입력해주세요.';
-  pattern?: {
-    value: RegExp;
-    message: '올바른 이메일 주소가 아닙니다.' | '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.';
-  };
-  validate?: {
-    check: (val: string) => string | undefined;
-  };
-};
-
-export default function Input({ label, id, type, placeholder, validation }: InputProps) {
+export default function Input({ label, id, type, placeholder, validation }: Props) {
   const {
     register,
     formState: { errors },
