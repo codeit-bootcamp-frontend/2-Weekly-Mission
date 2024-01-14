1a1e1b98f004c3ca922bfdb3235636e4cd969de1
박지원 <jackgg12322@gmail.com>
2024-01-14 19:08:23 +0900

    feat : api 함수 타입스크립트 타입 선언

--- src/api.ts
+++ src/api.ts
@@ -1,41 +1,34 @@
-const USER_URL = "https://bootcamp-api.codeit.kr/api/sample/";
-const FOLDER_URL = "https://bootcamp-api.codeit.kr/api/users/1";
+import type { FolderHeaderT,UserFolderT, UserHeaderT,FolderButtonsT, FolderLinksT  } from "../src/apiType"
 
-interface UserT {
-  id: number;
-  name : string;
-  email : string;
-  profileImageSource : string;
-}
+const USER_URL = 'https://bootcamp-api.codeit.kr/api/sample/';
+const FOLDER_URL = 'https://bootcamp-api.codeit.kr/api/users/1';
 
 
-
-
-export async function HeaderApi() :Promise<UserT>{
+export async function HeaderApi(): Promise<UserHeaderT> {
   const query = await fetch(`${USER_URL}user`);
   const response = await query.json();
   return response;
 }
 
-export async function LoginProfile() {
+export async function LoginProfile() : Promise<UserFolderT>{
   const query = await fetch(`${USER_URL}folder`);
   const response = await query.json();
   return response;
 }
 
-export async function FolderHeaderApi(){
+export async function FolderHeaderApi() : Promise<FolderHeaderT>{
   const query = await fetch(`${FOLDER_URL}`);
   const response = await query.json();
   return response;
 }
 
-export async function FolderButtonApi() {
-   const query = await fetch(`${FOLDER_URL}/folders`);
-   const response = await query.json();
-   return response;
+export async function FolderButtonApi() : Promise<FolderButtonsT>{
+  const query = await fetch(`${FOLDER_URL}/folders`);
+  const response = await query.json();
+  return response;
 }
 
-export async function FolderLoginProfile() {
+export async function FolderLoginProfile() : Promise<FolderLinksT> {
   const query = await fetch(`${FOLDER_URL}/links`);
   const response = await query.json();
   return response;
