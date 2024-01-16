505ff34cc5e857dba5874905fe4975fa4d91549f
lhv0829 <51733830+lhv0829@users.noreply.github.com>
2024-01-17 03:21:39 +0900

    Fix: 로그인, 회원가입 페이지에서 nav와 footer가 없기 때문에 공통으로 적용되는 부분을 folder 페이지와 shared 페이지에 별도로 적용

--- pages/folder/index.tsx
+++ pages/folder/index.tsx
@@ -1,8 +1,14 @@
+import Footer from "@/src/common/Components/Footer/Footer";
+import Nav from "@/src/common/Components/Nav/Nav";
 import FolderPage from "@/src/folder/Components/FolderPage";
 
 export default function Home () {
   return(
-    <FolderPage />
+    <>
+      <Nav />
+      <FolderPage />
+      <Footer />
+    </>
   )
 }
 
--- src/home/Components/Test.tsx
+++ src/home/Components/Test.tsx
@@ -4,8 +4,10 @@ const Test = () => {
   return(
     <main>
       <div className='flex justify-center items-center mx-auto gap-16 mt-20'>
-        <Link href='/shared' className='w-40 bg-indigo-500 text-white text-center text-4xl rounded-md'>Shared Page</Link>
-        <Link href='/folder' className='w-40 bg-indigo-500 text-white text-center text-4xl rounded-md'>Folder Page</Link>
+        <Link href='/shared' className='w-40 h-20 bg-indigo-500 text-white text-center text-4xl rounded-md'>Shared Page</Link>
+        <Link href='/folder' className='w-40 h-20 bg-indigo-500 text-white text-center text-4xl rounded-md'>Folder Page</Link>
+        <Link href='/signin' className='w-40 h-20 flex items-center justify-center bg-indigo-500 text-white text-center text-4xl rounded-md'>SignIn</Link>
+        <Link href='/signup' className='w-40 h-20 flex items-center justify-center bg-indigo-500 text-white text-center text-4xl rounded-md'>Signup</Link>
       </div>
     </main>
   )
