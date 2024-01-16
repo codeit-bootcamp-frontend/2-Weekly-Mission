617e70b4e24f0b347b0a88cf8eeb8c661eb40c8a
JunCastle <juncastle97@gmail.com>
2024-01-16 16:52:44 +0900

    Fix: 이미지 권한 허용

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  images: {
+    domains: [
+      "codeit.kr",
+      "codeit-images.codeit.com",
+      "codeit-frontend.codeit.com",
+      "tanstack.com",
+      "s.pstatic.net",
+      "storybook.js.org",
+      "assets.vercel.com",
+      "reactjs.org",
+      "jasonwatmore.com",
+    ],
+  },
+};
