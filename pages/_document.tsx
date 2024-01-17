8e5033e22276bf5f18c5b8f62e0b9415035fcb0d
Yoon-kyungLee <yk2588@nate.com>
2024-01-17 17:48:33 +0900

    fix: 모달창 중앙 정렬 오류 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,11 +2,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ko">
       <Head />
       <body>
         <Main />
-        <div id="portal" />
         <NextScript />
       </body>
     </Html>
