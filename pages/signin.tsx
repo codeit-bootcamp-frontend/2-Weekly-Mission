f9e909bfae1b93c75ad15c30609d2cd4b2cb01be
Chyyni <catharina0409@gmail.com>
2024-01-17 19:07:49 +0900

    feat: 로그인, 회원가입 페이지에서 토큰이 없을 경우에만 페이지가 보이도록 수정

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 import SignLayout from '@/components/Sign/SignLayout/SignLayout';
@@ -7,24 +7,31 @@ import SignForm from '@/components/Sign/SignForm/SignForm';
 import Social from '@/components/Sign/Social/Social';
 import EmailInput from '@/components/Sign/EmailInput/EmailInput';
 import PasswordInput from '@/components/Sign/PasswordInput/PasswordInput';
+import Loading from '@/components/Loading/Loading';
 
 export default function Signin() {
+  const [displayPage, setDisplayPage] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
-    if (localStorage.getItem('accessToken')) {
-      router.push('/folder');
-    }
+    const accessToken = localStorage.getItem('accessToken');
+    accessToken ? router.push('/folder') : setDisplayPage(true);
   }, []);
 
   return (
-    <SignLayout>
-      <SignHeader question='회원이 아니신가요?' href='/signup' linkText='회원 가입하기' />
-      <SignForm url='/sign-in' cta='로그인'>
-        <EmailInput />
-        <PasswordInput />
-      </SignForm>
-      <Social text='소셜 로그인' />
-    </SignLayout>
+    <>
+      {displayPage ? (
+        <SignLayout>
+          <SignHeader question='회원이 아니신가요?' href='/signup' linkText='회원 가입하기' />
+          <SignForm url='/sign-in' cta='로그인'>
+            <EmailInput />
+            <PasswordInput />
+          </SignForm>
+          <Social text='소셜 로그인' />
+        </SignLayout>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 }
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 import SignLayout from '@/components/Sign/SignLayout/SignLayout';
@@ -8,24 +8,31 @@ import Social from '@/components/Sign/Social/Social';
 import EmailInput from '@/components/Sign/EmailInput/EmailInput';
 import PasswordInput from '@/components/Sign/PasswordInput/PasswordInput';
 import DoubleCheckPasswordInput from '@/components/Sign/DoubleCheckPasswordInput/DoubleCheckPasswordInput';
+import Loading from '@/components/Loading/Loading';
 
 export default function Signup() {
+  const [displayPage, setDisplayPage] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
-    if (localStorage.getItem('accessToken')) {
-      router.push('/folder');
-    }
+    const accessToken = localStorage.getItem('accessToken');
+    accessToken ? router.push('/folder') : setDisplayPage(true);
   }, []);
   return (
-    <SignLayout>
-      <SignHeader question='이미 회원이신가요?' href='/signin' linkText='로그인 하기' />
-      <SignForm url='/sign-up' cta='회원가입'>
-        <EmailInput />
-        <PasswordInput />
-        <DoubleCheckPasswordInput />
-      </SignForm>
-      <Social text='다른 방식으로 가입하기' />
-    </SignLayout>
+    <>
+      {displayPage ? (
+        <SignLayout>
+          <SignHeader question='이미 회원이신가요?' href='/signin' linkText='로그인 하기' />
+          <SignForm url='/sign-up' cta='회원가입'>
+            <EmailInput />
+            <PasswordInput />
+            <DoubleCheckPasswordInput />
+          </SignForm>
+          <Social text='다른 방식으로 가입하기' />
+        </SignLayout>
+      ) : (
+        <Loading />
+      )}
+    </>
   );
 }
