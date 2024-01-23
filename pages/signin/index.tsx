645d1d547fef91cc642886fa4e6a4c8b3ec1226c
jeongil Lee <79638133+201steve@users.noreply.github.com>
2024-01-23 21:40:43 +0900

    Revert "[남숙희] Week13"

--- pages/signin/index.tsx
+++
@@ -1,7 +0,0 @@
-import { SignLayout } from "@/components/page-layout/SignLayout/SignLayout";
-import { SignInForm } from "@/components/sign/ui-sign-in-form/SignInForm";
-import { SignHeader } from "@/components/sign/ui-sign-header/SignHeader";
-
-export default function Signin() {
-  return <SignLayout header={<SignHeader />} signForm={<SignInForm />} />;
-}
--- pages/signup/index.tsx
+++
@@ -1,7 +0,0 @@
-import { SignLayout } from "@/components/page-layout/SignLayout/SignLayout";
-import { SignHeader } from "@/components/sign/ui-sign-header/SignHeader";
-import { SignUpForm } from "@/components/sign/ui-sign-up-form/SignUpForm";
-
-export default function Signup() {
-  return <SignLayout header={<SignHeader />} signForm={<SignUpForm />} />;
-}
