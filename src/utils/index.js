4f7b666696db905a03c2d9859aad4d10d29537ac
designsoo <kheesoo10@gmail.com>
2024-02-26 02:06:09 +0900

    feat: Implement navigate utile function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 export * from './getDateDiff';
 export * from './getFormatDate';
 export * from './handleOutsideClick';
+export * from './navigate';
 export * from './redirectToPage';
 export * from './shareKaKaoLink';
