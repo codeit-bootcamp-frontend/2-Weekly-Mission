304245c940d311494820eac8181e7765652c0bc2
designsoo <kheesoo10@gmail.com>
2024-02-26 01:59:08 +0900

    remove: Delete unused files

--- src/components/common/Overlay/index.jsx
+++
@@ -1,8 +0,0 @@
-import styles from './Overlay.module.scss';
-import classNames from 'classnames/bind';
-
-const cx = classNames.bind(styles);
-
-export const Overlay = ({ children }) => {
-  return <div className={cx('overlay')}>{children}</div>;
-};
--- src/components/layout/AuthLayout/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './AuthLayout.module.scss';
-import classNames from 'classnames/bind';
-
-const cx = classNames.bind(styles);
-
-const AuthLayout = ({ children }) => {
-  return <div className={cx('auth-layout')}>{children}</div>;
-};
-export default AuthLayout;
