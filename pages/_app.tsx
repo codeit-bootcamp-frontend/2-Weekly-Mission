8ba57b9941020f1f536f7053d4a170201e75e65d
CheeseB <2489ckckck@naver.com>
2024-02-28 00:27:10 +0900

    feat: 리액트 쿼리 설치 및 초기 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import "@/styles/reset.css";
-import type { AppProps } from "next/app";
+import '@/styles/reset.css';
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
