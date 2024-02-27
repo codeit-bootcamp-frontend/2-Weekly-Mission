41c73032b1340045179624d718db305c063852c2
CheeseB <2489ckckck@naver.com>
2024-02-28 01:04:40 +0900

    feat: signin 기능에 useMutation 적용

--- src/auth/data-access-auth/useSignIn.tsx
+++ src/auth/data-access-auth/useSignIn.tsx
@@ -1,31 +1,36 @@
-import { axiosInstance, useAsync } from "@/src/sharing/util";
-import { useCallback, useEffect } from "react";
-import { Token } from "../type";
+import { axiosInstance, useAsync } from '@/src/sharing/util';
+import { useCallback, useEffect } from 'react';
+import { Token } from '../type';
+import { useMutation } from '@tanstack/react-query';
 
 type UseSignInParams = { email: string; password: string };
 
 export const useSignIn = ({ email, password }: UseSignInParams) => {
   const signIn = useCallback(
     () =>
-      axiosInstance.post<{ data: Token }>("sign-in", {
+      axiosInstance.post<Token>('sign-in', {
         email,
         password,
       }),
     [email, password]
   );
-  const { execute, loading, error, data } = useAsync({ asyncFunction: signIn, lazyMode: true });
+
+  const { data, isPending, isError, mutate } = useMutation({
+    mutationFn: signIn,
+  });
+
   const accessToken = data?.data.accessToken;
 
   useEffect(() => {
     if (accessToken) {
-      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem('accessToken', accessToken);
     }
   }, [accessToken]);
 
   return {
-    execute,
-    loading,
-    error,
+    mutate,
+    isPending,
+    isError,
     data,
   };
 };
--- src/auth/feature-sign-in-form/SignInForm.tsx
+++ src/auth/feature-sign-in-form/SignInForm.tsx
@@ -1,45 +1,51 @@
-import { Input } from "@/src/sharing/ui-input";
-import { PasswordInput } from "@/src/sharing/ui-password-input";
-import { Controller, useForm } from "react-hook-form";
-import { useSignIn } from "../data-access-auth";
-import { ERROR_MESSAGE, PLACEHOLDER } from "./constant";
-import { useEffect } from "react";
-import styles from "./SignInForm.module.scss";
-import classNames from "classnames/bind";
-import { Cta } from "@/src/sharing/ui-cta";
-import { useTokenRedirect } from "../util-use-token-redirect";
+import { Input } from '@/src/sharing/ui-input';
+import { PasswordInput } from '@/src/sharing/ui-password-input';
+import { Controller, useForm } from 'react-hook-form';
+import { useSignIn } from '../data-access-auth';
+import { ERROR_MESSAGE, PLACEHOLDER } from './constant';
+import { useEffect } from 'react';
+import styles from './SignInForm.module.scss';
+import classNames from 'classnames/bind';
+import { Cta } from '@/src/sharing/ui-cta';
+import { useTokenRedirect } from '../util-use-token-redirect';
 
 const cx = classNames.bind(styles);
 
 export const SignInForm = () => {
   const { control, handleSubmit, watch, setError } = useForm({
-    defaultValues: { email: "", password: "" },
-    mode: "onBlur",
+    defaultValues: { email: '', password: '' },
+    mode: 'onBlur',
   });
-  const { execute, data, error } = useSignIn({
-    email: watch("email"),
-    password: watch("password"),
+  const { mutate, data, isError } = useSignIn({
+    email: watch('email'),
+    password: watch('password'),
   });
 
   useTokenRedirect(data?.data.accessToken);
 
   useEffect(() => {
-    if (error) {
-      setError("email", { type: "invalid", message: ERROR_MESSAGE.emailCheck });
-      setError("password", { type: "invalid", message: ERROR_MESSAGE.passwordCheck });
+    if (isError) {
+      setError('email', { type: 'invalid', message: ERROR_MESSAGE.emailCheck });
+      setError('password', {
+        type: 'invalid',
+        message: ERROR_MESSAGE.passwordCheck,
+      });
     }
-  }, [error, setError]);
+  }, [isError, setError]);
 
   return (
-    <form className={cx("form")} onSubmit={handleSubmit(execute)}>
-      <div className={cx("input-box")}>
-        <label className={cx("label")}>이메일</label>
+    <form className={cx('form')} onSubmit={handleSubmit(() => mutate())}>
+      <div className={cx('input-box')}>
+        <label className={cx('label')}>이메일</label>
         <Controller
           control={control}
-          name="email"
+          name='email'
           rules={{
             required: ERROR_MESSAGE.emailRequired,
-            pattern: { value: /\S+@\S+\.\S+/, message: ERROR_MESSAGE.emailInvalid },
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: ERROR_MESSAGE.emailInvalid,
+            },
           }}
           render={({ field, fieldState }) => (
             <Input
@@ -51,11 +57,11 @@ export const SignInForm = () => {
           )}
         />
       </div>
-      <div className={cx("input-box")}>
-        <label className={cx("label")}>비밀번호</label>
+      <div className={cx('input-box')}>
+        <label className={cx('label')}>비밀번호</label>
         <Controller
           control={control}
-          name="password"
+          name='password'
           rules={{ required: ERROR_MESSAGE.passwordRequired }}
           render={({ field, fieldState }) => (
             <PasswordInput
@@ -68,7 +74,7 @@ export const SignInForm = () => {
           )}
         />
       </div>
-      <button className={cx("button")} type="submit">
+      <button className={cx('button')} type='submit'>
         <Cta>로그인</Cta>
       </button>
     </form>
