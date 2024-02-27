bc5dd8a8483db3eac1e05a52125d4d2d2aa1d328
CheeseB <2489ckckck@naver.com>
2024-02-28 01:09:25 +0900

    feat: signup 기능에 useMutation 적용

--- src/auth/data-access-auth/useSignUp.tsx
+++ src/auth/data-access-auth/useSignUp.tsx
@@ -1,31 +1,35 @@
-import { axiosInstance, useAsync } from "@/src/sharing/util";
-import { useCallback, useEffect } from "react";
-import { Token } from "../type";
+import { axiosInstance, useAsync } from '@/src/sharing/util';
+import { useCallback, useEffect } from 'react';
+import { Token } from '../type';
+import { useMutation } from '@tanstack/react-query';
 
 type UseSignUpParams = { email: string; password: string };
 
 export const useSignUp = ({ email, password }: UseSignUpParams) => {
   const signUp = useCallback(
     () =>
-      axiosInstance.post<{ data: Token }>("sign-up", {
+      axiosInstance.post<Token>('sign-up', {
         email,
         password,
       }),
     [email, password]
   );
-  const { execute, loading, error, data } = useAsync({ asyncFunction: signUp, lazyMode: true });
+  const { data, isPending, isError, mutate } = useMutation({
+    mutationFn: signUp,
+  });
+
   const accessToken = data?.data.accessToken;
 
   useEffect(() => {
     if (accessToken) {
-      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem('accessToken', accessToken);
     }
   }, [accessToken]);
 
   return {
-    execute,
-    loading,
-    error,
+    mutate,
+    isPending,
+    isError,
     data,
   };
 };
--- src/auth/feature-sign-up-form/SignUpForm.tsx
+++ src/auth/feature-sign-up-form/SignUpForm.tsx
@@ -1,42 +1,46 @@
-import { Input } from "@/src/sharing/ui-input";
-import { PasswordInput } from "@/src/sharing/ui-password-input";
-import { Controller, useForm } from "react-hook-form";
-import { useCheckEmailDuplicate, useSignUp } from "../data-access-auth";
-import { ERROR_MESSAGE, PLACEHOLDER } from "./constant";
-import styles from "./SignUpForm.module.scss";
-import classNames from "classnames/bind";
-import { Cta } from "@/src/sharing/ui-cta";
-import { useTokenRedirect } from "../util-use-token-redirect";
+import { Input } from '@/src/sharing/ui-input';
+import { PasswordInput } from '@/src/sharing/ui-password-input';
+import { Controller, useForm } from 'react-hook-form';
+import { useCheckEmailDuplicate, useSignUp } from '../data-access-auth';
+import { ERROR_MESSAGE, PLACEHOLDER } from './constant';
+import styles from './SignUpForm.module.scss';
+import classNames from 'classnames/bind';
+import { Cta } from '@/src/sharing/ui-cta';
+import { useTokenRedirect } from '../util-use-token-redirect';
 
 const cx = classNames.bind(styles);
 
 export const SignUpForm = () => {
   const { control, handleSubmit, watch } = useForm({
-    defaultValues: { email: "", password: "", confirmedPassword: "" },
-    mode: "onBlur",
-    reValidateMode: "onBlur",
+    defaultValues: { email: '', password: '', confirmedPassword: '' },
+    mode: 'onBlur',
+    reValidateMode: 'onBlur',
   });
-  const { execute: checkEmailDuplicate } = useCheckEmailDuplicate(watch("email"));
-  const { execute: signUp, data } = useSignUp({
-    email: watch("email"),
-    password: watch("password"),
+  const { execute } = useCheckEmailDuplicate(watch('email'));
+
+  const { mutate, data } = useSignUp({
+    email: watch('email'),
+    password: watch('password'),
   });
 
   useTokenRedirect(data?.data.accessToken);
 
   return (
-    <form className={cx("form")} onSubmit={handleSubmit(signUp)}>
-      <div className={cx("input-box")}>
-        <label className={cx("label")}>이메일</label>
+    <form className={cx('form')} onSubmit={handleSubmit(() => mutate())}>
+      <div className={cx('input-box')}>
+        <label className={cx('label')}>이메일</label>
         <Controller
           control={control}
-          name="email"
+          name='email'
           rules={{
             required: ERROR_MESSAGE.emailRequired,
-            pattern: { value: /\S+@\S+\.\S+/, message: ERROR_MESSAGE.emailInvalid },
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: ERROR_MESSAGE.emailInvalid,
+            },
             validate: {
               alreadyExist: async () => {
-                const response = await checkEmailDuplicate();
+                const response = await execute();
                 if (!response?.data?.data.isUsableNickname) {
                   return ERROR_MESSAGE.emailAlreadyExist;
                 }
@@ -54,11 +58,11 @@ export const SignUpForm = () => {
           )}
         />
       </div>
-      <div className={cx("input-box")}>
-        <label className={cx("label")}>비밀번호</label>
+      <div className={cx('input-box')}>
+        <label className={cx('label')}>비밀번호</label>
         <Controller
           control={control}
-          name="password"
+          name='password'
           rules={{
             required: ERROR_MESSAGE.passwordInvalid,
             pattern: {
@@ -77,15 +81,15 @@ export const SignUpForm = () => {
           )}
         />
       </div>
-      <div className={cx("input-box")}>
-        <label className={cx("label")}>비밀번호 확인</label>
+      <div className={cx('input-box')}>
+        <label className={cx('label')}>비밀번호 확인</label>
         <Controller
           control={control}
-          name="confirmedPassword"
+          name='confirmedPassword'
           rules={{
             validate: {
               isMatch: (value) => {
-                if (value !== watch("password")) {
+                if (value !== watch('password')) {
                   return ERROR_MESSAGE.confirmedPasswordNotMatch;
                 }
                 return true;
@@ -103,7 +107,7 @@ export const SignUpForm = () => {
           )}
         />
       </div>
-      <button className={cx("button")} type="submit">
+      <button className={cx('button')} type='submit'>
         <Cta>회원가입</Cta>
       </button>
     </form>
